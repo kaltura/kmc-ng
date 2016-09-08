4f20e82dafc722e118eacb77b8f7741de3c451b5
Eran Sakal <eran@sakalim.com>
2016-09-08 16:33:57 +0300

    Kmc shared libraries (#14)
    refactor: move can-activate elements to the libraries and fix modules declaration

--- src/app/kmc-shell/kmc-shell.module.ts
+++ src/app/kmc-shell/kmc-shell.module.ts
@@ -1,9 +1,13 @@
 import { NgModule, Injectable } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
 import { CommonModule }       from '@angular/common';
+import { KMCngCoreModule, AppStorage } from '@kaltura/kmcng-core';
+import { KalturaApiModule } from '@kaltura/kaltura-api';
 import { KMCngShellCoreModule } from '@kaltura/kmcng-shell';
-import { GetConfigPostLoadProvider, PostLoadAdapter } from '@kaltura/kmcng-core';
-import { KalturaAPIConfig } from '@kaltura/kaltura-api';
+import { GetConfigPostLoadProvider } from '@kaltura/kmcng-core';
+import { NG2_WEBSTORAGE } from 'ng2-webstorage';
 
 import { DashboardComponent } from "./components/dashboard/dashboard.component";
 import {AppMenuComponent} from "./components/app-menu/app-menu.component";
@@ -11,24 +15,20 @@ import {LanguageMenuComponent} from "./components/language-menu/language-menu.co
 import {LoginComponent} from "./components/login/login.component";
 import {UploadComponent} from "./components/upload/upload.component";
 import {UserSettingsComponent} from "./components/user-settings/user-settings.component";
+import { BrowserService } from '@kaltura/kmcng-shell';
 
-@Injectable()
-class KalturaAPIConfigConfigAdapter implements PostLoadAdapter
-{
-  constructor(private kalturaAPIConfig : KalturaAPIConfig){
-
-  }
-  execute(config : any) : void{
-    // TODO [kmc] handle error scenarios (missing core.kaltura)
-    const { apiUrl, apiVersion }  = config.core.kaltura;
-    const kalturaAPIConfig  = this.kalturaAPIConfig;
-    kalturaAPIConfig.apiUrl = apiUrl;
-    kalturaAPIConfig.apiVersion = apiVersion;
-  }
-}
+import { KalturaAPIConfigAdapter } from './shared/kaltura-api-config-adapter.service';
 
 @NgModule({
-  imports:      [ CommonModule, KMCngShellCoreModule, RouterModule.forChild([]) ],
+  imports:      [
+    CommonModule,
+    KMCngShellCoreModule,
+    RouterModule.forChild([]),
+    BrowserModule,
+    HttpModule,
+    KMCngCoreModule,
+    KalturaApiModule,
+    ],
   declarations: [
     DashboardComponent,
     AppMenuComponent,
@@ -38,7 +38,8 @@ class KalturaAPIConfigConfigAdapter implements PostLoadAdapter
     UserSettingsComponent ],
   exports: [DashboardComponent,LoginComponent ],
   providers:    [
-    GetConfigPostLoadProvider(KalturaAPIConfigConfigAdapter)
+    GetConfigPostLoadProvider(KalturaAPIConfigAdapter),
+    NG2_WEBSTORAGE
   ]
 })
 export class KMCShellAppModule { }
