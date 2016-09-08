f6226ccb2bf7ffb574b8a97ed7c2c2927c3a8718
amirch1 <amir.chervinsky@kaltura.com>
2016-09-08 15:42:16 +0300

    merge dev

--- src/app/kmc-apps/content-playlists-app/content-playlists-app.module.ts
+++ src/app/kmc-apps/content-playlists-app/content-playlists-app.module.ts
@@ -6,9 +6,10 @@ import { routing} from './content-playlists-app.routes';
 import { PlaylistsComponent } from './components/playlists.component';
 import { PlaylistTypePipe } from './pipes/playlist.type.pipe';
 import { KMCngShellCommonModule } from '@kaltura/kmcng-shell';
+import { DropdownModule } from 'ng2-bootstrap/ng2-bootstrap';
 
 @NgModule({
-  imports:      [ CommonModule, FormsModule, routing, ReactiveFormsModule, KMCngShellCommonModule ],
+  imports:      [ CommonModule, FormsModule, routing, ReactiveFormsModule, KMCngShellCommonModule, DropdownModule ],
   declarations: [ PlaylistsComponent, PlaylistTypePipe ],
   providers:    []
 })
