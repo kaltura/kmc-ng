24b021e499d99c7bf958f4379947fdcc0d807d4b
amirch1 <amir.chervinsky@kaltura.com>
2016-09-29 11:39:42 +0300

    use primeng text input style

--- src/app/kmc-apps/content-entries-app/components/entries.component.ts
+++ src/app/kmc-apps/content-entries-app/components/entries.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
-import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { KalturaAPIClient } from '@kaltura/kaltura-api';
 import { BaseEntryService } from '@kaltura/kaltura-api/base-entry';
 
