271d97d50a3eea091df226ee40453515c9db4918
Eran Sakal <eran.sakal@kaltura.com>
2016-10-09 12:21:02 +0300

    refactor: rename src/app inner folders name

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes, RouterModule } from '@angular/router';
 import { AuthCanActivate, AppBootstrap } from '@kaltura-ng2/kaltura-common';
 
-import { LoginComponent } from './kmc-shell/components/login/login.component';
-import { DashboardComponent } from './kmc-shell/components/dashboard/dashboard.component';
-import {ErrorComponent} from "./kmc-shell/components/error/error.component";
+import { LoginComponent } from './shell/components/login/login.component';
+import { DashboardComponent } from './shell/components/dashboard/dashboard.component';
+import {ErrorComponent} from "./shell/components/error/error.component";
 
 
 const routes: Routes = <Routes>[
@@ -21,28 +21,28 @@ const routes: Routes = <Routes>[
           { path: '', redirectTo: 'entries', pathMatch: 'full' },
           { path: 'entries', loadChildren: () => new Promise(resolve => {
             (require as any).ensure([], require => {
-              resolve(require('./kmc-apps/content-entries-app/content-entries-app.module').ContentEntriesAppModule);
+              resolve(require('./applications/content-entries-app/content-entries-app.module').ContentEntriesAppModule);
             });
           })},
           { path: 'moderation', loadChildren: () => new Promise(resolve => {
             (require as any).ensure([], require => {
-              resolve(require('./kmc-apps/content-moderation-app/content-moderation-app.module').ContentModerationAppModule);
+              resolve(require('./applications/content-moderation-app/content-moderation-app.module').ContentModerationAppModule);
             });
           })},
           { path: 'playlists', loadChildren: () => new Promise(resolve => {
             (require as any).ensure([], require => {
-              resolve(require('./kmc-apps/content-playlists-app/content-playlists-app.module').ContentPlaylistsAppModule);
+              resolve(require('./applications/content-playlists-app/content-playlists-app.module').ContentPlaylistsAppModule);
             });
           })}
         ]},
         { path: 'dashboard', loadChildren: () => new Promise(resolve => {
           (require as any).ensure([], require => {
-            resolve(require('./kmc-apps/dashboard-app/dashboard-app.module').DashboardAppModule);
+            resolve(require('./applications/dashboard-app/dashboard-app.module').DashboardAppModule);
           });
         })},
         { path: 'studio', loadChildren: () => new Promise(resolve => {
           (require as any).ensure([], require => {
-            resolve(require('./kmc-apps/studio-universal-app/studio-universal-app.module').StudioUniversalAppModule);
+            resolve(require('./applications/studio-universal-app/studio-universal-app.module').StudioUniversalAppModule);
           });
         })}
       ]
