d479193aa3991e406446a20c412ea76e17a7bb51
amirch1 <amir.chervinsky@kaltura.com>
2016-11-08 11:12:00 +0200

    KMCNG-61: 1. Create hierarchial data from received data 2. Render data into a checkbox tree component 3. Emit change event upon category selection

--- src/shared/kmc-content-ui/categories-filter/categories-filter.component.ts
+++ src/shared/kmc-content-ui/categories-filter/categories-filter.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit, OnDestroy, } from '@angular/core';
+import { Component, OnInit, OnDestroy, EventEmitter, Output} from '@angular/core';
 
 import { Subscription} from 'rxjs';
+import * as R from 'ramda';
 
-import { ContentCategoriesStore, Categories } from 'kmc-content-ui/providers/content-categories-store.service';
+import { ContentCategoriesStore, Category } from 'kmc-content-ui/providers/content-categories-store.service';
 
 @Component({
   selector: 'kmc-categories-filter',
@@ -13,16 +14,21 @@ export class CategoriesFilterComponent implements OnInit, OnDestroy{
 
   loading = false;
   categories: any;
+  categoriesMap: {};
   categoriesSubscribe : Subscription;
+
+  @Output()
+  categoriesChanged = new EventEmitter<any>();
+
   constructor(public contentCategoriesStore: ContentCategoriesStore) {
 
   }
 
   ngOnInit() {
-
     this.categoriesSubscribe = this.contentCategoriesStore.categories$.subscribe(
-      (categories: Categories) => {
-        this.categories = categories ? categories.items : {};
+      (categoriesRoot: any) => {
+        this.categories = categoriesRoot.items ? categoriesRoot.items : [];
+        this.categoriesMap = categoriesRoot.map;
       },
       (error) => {
         // TODO [KMC] - handle error
@@ -41,6 +47,44 @@ export class CategoriesFilterComponent implements OnInit, OnDestroy{
 
   ngOnDestroy(){
     this.categoriesSubscribe.unsubscribe();
+  }
+
+  categorySelected(toggleCat: Category){
+    // select / unselect all children (recursive)
+    let toggleChildren = (cat: Category) => {
+      cat.children.forEach(function(category: Category) {
+        category.selected = cat.selected;
+        if (category.children.length){
+          toggleChildren(category);
+        }
+      });
+    }
+    toggleChildren(toggleCat);
+
+    // update parent if needed (recursive)
+    let updateParent = (cat: Category) => {
+      if (cat.parentId != "0") {
+        const parentCat = this.categoriesMap[cat.parentId];
+        const siblings = parentCat.children;
+        parentCat.partialSelection = false;
+        if ( R.find(R.propEq('selected', !cat.selected))(siblings) === undefined){
+          parentCat.selected = cat.selected;
+          if (parentCat.parentId != "0"){
+            updateParent(parentCat);
+          }
+        }else{
+          parentCat.partialSelection = true;
+        }
+      }
+    }
+    updateParent(toggleCat);
+    let selectedCategories = [];
+    for (let key in this.categoriesMap){
+      if (this.categoriesMap[key].selected){
+        selectedCategories.push(key);
+      }
+    }
+    this.categoriesChanged.emit(selectedCategories);
   }
 
 }
--- src/shared/kmc-content-ui/providers/content-categories-store.service.ts
+++ src/shared/kmc-content-ui/providers/content-categories-store.service.ts
@@ -3,14 +3,28 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Observable } from 'rxjs/Observable';
 
 import { KalturaAPIClient } from '@kaltura-ng2/kaltura-api';
-import { CategoryService, KalturaCategoryFilter } from '@kaltura-ng2/kaltura-api/category';
+import { CategoryService, KalturaCategoryFilter, KalturaDetachedResponseProfile } from '@kaltura-ng2/kaltura-api/category';
 
 import * as R from 'ramda';
 
 export interface Categories{
-    items : any[],
+    items : Category[],
+    map: any,
     loaded : boolean,
     status: string
+}
+
+export class Category {
+  selected = false;
+  partialSelection = false;
+  label = "";
+  parentId = "";
+  children = [];
+
+  constructor(label: string, parentId: string){
+    this.label = label;
+    this.parentId = parentId;
+  }
 }
 
 @Injectable()
@@ -18,9 +32,10 @@ export class ContentCategoriesStore
 {
     // TODO [KMC] - clear cached data on logout
 
-    private _categories: BehaviorSubject<Categories> = new BehaviorSubject({items:[],loaded: false, status: ''});
-
+    private _categories: BehaviorSubject<Categories> = new BehaviorSubject({items: [], map: {}, loaded: false, status: ''});
     public categories$: Observable<Categories> = this._categories.asObservable();
+
+    private responseProfile: any;
 
     constructor(private kalturaAPIClient : KalturaAPIClient) {
 
@@ -29,25 +44,37 @@ export class ContentCategoriesStore
 
     public reloadCategories(ignoreCache: boolean = false) : Observable<boolean>
     {
-        let filter;
+        let filter, responseProfile;
+        let categoriesMap = {};
 
         filter = new KalturaCategoryFilter();
         Object.assign(filter, {orderBy : '+name'});
 
-        const cateogires = this._categories.getValue();
+        responseProfile = new KalturaDetachedResponseProfile();
+        Object.assign(responseProfile, {
+          "objectType": "KalturaDetachedResponseProfile",
+          "type": "1",
+          "fields": "id,parentId,name"
+        });
 
-      if (ignoreCache || !cateogires.loaded || cateogires.status) {
-        this._categories.next({items: [], loaded: false, status: ''});
+        const categories = this._categories.getValue();
+
+      if (ignoreCache || !categories.loaded || categories.status) {
+        this._categories.next({items: [], map: {}, loaded: false, status: ''});
 
         return Observable.create(observe => {
-          CategoryService.list(filter)
+          CategoryService.list(filter, responseProfile)
             .execute(this.kalturaAPIClient)
-            .map(function(response) {
-              return response;
+            .map((response) => {
+              if (response && response.objects){
+                return this.buildCategoriesHyrarchy(response.objects, categoriesMap);
+              }else{
+                return [];
+              }
             })
             .subscribe(
-              (response) => {
-                this._categories.next({items: <any[]>response.objects, loaded: true, status: ''});
+              (categories) => {
+                this._categories.next({items: <Category[]>categories, map: categoriesMap, loaded: true, status: ''});
                 observe.next(true);
                 observe.complete();
               },
@@ -62,6 +89,31 @@ export class ContentCategoriesStore
         return Observable.of(true);
       }
     }
+
+
+  buildCategoriesHyrarchy(categories: any[], categoriesMap) : any[] {
+    // convert flat array to hyrarchial data array
+    let allCategories = [];
+    let rootLevel = [];
+
+    categories.sort(function(a,b) {return (a.name.toLocaleLowerCase() > b.name.toLocaleLowerCase()) ? 1 : ((b.name.toLocaleLowerCase() > a.name.toLocaleLowerCase()) ? -1 : 0);} );
+
+    categories.forEach(function(category: any) {
+      let cat = new Category(category.name, category.parentId);
+      categoriesMap[category.id] = cat;
+      allCategories.push(cat);
+    }, this);
+
+    allCategories.forEach(function(category: Category) {
+      if (category.parentId == "0"){
+        rootLevel.push(category);
+      }else{
+        categoriesMap[category.parentId].children.push(category);
+      }
+    }, this);
+
+    return rootLevel;
+  }
 
 }
 
