5d7e544c1de88db0a068ea600c6f29dd3b999f8f
Eran Sakal <eran.sakal@kaltura.com>
2016-11-16 17:39:08 +0200

    fixing entries to match new kaltura api

--- src/shared/kmc-content-ui/pipes/entry-type.pipe.ts
+++ src/shared/kmc-content-ui/pipes/entry-type.pipe.ts
@@ -2,27 +2,30 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({name: 'entryType'})
 export class EntryTypePipe implements PipeTransform {
-  transform(value = '1'): string {
+  transform(value): string {
     let className = 'k-entry-';
-    switch (value.toString()) {
-      case '1':
-        className += 'media';
-         break;
-      case '2':
-        className += 'image';
-        break;
-      case '5':
-        className += 'audio';
-        break;
-      case '201':
-      case '202':
-      case '203':
-      case '204':
-        className += 'live';
-        break;
-      default:
-        className += 'unknown';
-        break;
+
+    if (value) {
+      switch (value.toString()) {
+        case '1':
+          className += 'media';
+          break;
+        case '2':
+          className += 'image';
+          break;
+        case '5':
+          className += 'audio';
+          break;
+        case '201':
+        case '202':
+        case '203':
+        case '204':
+          className += 'live';
+          break;
+        default:
+          className += 'unknown';
+          break;
+      }
     }
     return className;
   }
