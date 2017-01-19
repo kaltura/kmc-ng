5908331a841bc7c495c4a9f66c67c91b900b1f95
amirch1 <amir.chervinsky@kaltura.com>
2017-01-19 09:16:57 +0200

    support localization in entry type and bulk actions

--- src/shared/kmc-content-ui/pipes/entry-status.pipe.ts
+++ src/shared/kmc-content-ui/pipes/entry-status.pipe.ts
@@ -1,50 +1,55 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import {Pipe, PipeTransform} from '@angular/core';
+import {AppLocalization} from '@kaltura-ng2/kaltura-common';
 
 @Pipe({name: 'entryStatus'})
 export class EntryStatusPipe implements PipeTransform {
-  transform(value: string): string {
-    let ret: string = '';
-      // TODO [kmc] use the actual enum instead of strings
-    if (typeof(value) !== 'undefined' && value !== null)  {
-        switch (value.toString()) {
-            case '-2':
-                ret = 'Error Importing';
-                break;
-            case '-1':
-                ret = 'Error Converting';
-                break;
-            case 'virusScan.ScanFailure':
-                ret = 'Scan Failure';
-                break;
-            case '0':
-                ret = 'Import';
-                break;
-            case 'virusScan.Infected':
-                ret = 'Infected';
-                break;
-            case '1':
-                ret = 'Preconvert';
-                break;
-            case '2':
-                ret = 'Ready';
-                break;
-            case '3':
-                ret = 'Deleted';
-                break;
-            case '4':
-                ret = 'Pending';
-                break;
-            case '5':
-                ret = 'Moderate';
-                break;
-            case '6':
-                ret = 'Blocked';
-                break;
-            case '7':
-                ret = 'No Content';
-                break;
-        }
+
+    constructor(private appLocalization: AppLocalization) {
     }
-    return ret;
-  }
+
+    transform(value: string): string {
+        let ret: string = '';
+        // TODO [kmc] use the actual enum instead of strings
+        if (typeof(value) !== 'undefined' && value !== null) {
+            switch (value.toString()) {
+                case '-2':
+                    ret = this.appLocalization.get("applications.content.entryStatus.errorImporting");
+                    break;
+                case '-1':
+                    ret = this.appLocalization.get("applications.content.entryStatus.errorConverting");
+                    break;
+                case 'virusScan.ScanFailure':
+                    ret = this.appLocalization.get("applications.content.entryStatus.scanFailure");
+                    break;
+                case '0':
+                    ret = this.appLocalization.get("applications.content.entryStatus.import");
+                    break;
+                case 'virusScan.Infected':
+                    ret = this.appLocalization.get("applications.content.entryStatus.infected");
+                    break;
+                case '1':
+                    ret = this.appLocalization.get("applications.content.entryStatus.preconvert");
+                    break;
+                case '2':
+                    ret = this.appLocalization.get("applications.content.entryStatus.ready");
+                    break;
+                case '3':
+                    ret = this.appLocalization.get("applications.content.entryStatus.deleted");
+                    break;
+                case '4':
+                    ret = this.appLocalization.get("applications.content.entryStatus.pending");
+                    break;
+                case '5':
+                    ret = this.appLocalization.get("applications.content.entryStatus.moderate");
+                    break;
+                case '6':
+                    ret = this.appLocalization.get("applications.content.entryStatus.blocked");
+                    break;
+                case '7':
+                    ret = this.appLocalization.get("applications.content.entryStatus.noContent");
+                    break;
+            }
+        }
+        return ret;
+    }
 }
