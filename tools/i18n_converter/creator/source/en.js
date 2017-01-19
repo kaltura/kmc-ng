317bc414b42461cb427b1609eb2a64a1d253c7cb
Amir Chervinsky <amir.chervinsky@kaltura.com>
2017-01-19 12:27:59 +0200

    feat: update localization for entries application and app shell

---
+++ tools/i18n_converter/creator/source/en.js
@@ -0,0 +1,44 @@
+var source = {
+    "app":{
+        "menu": {
+            "upload":"Upload",
+            "Dashboard": "Dashboard",
+            "Content": "Content",
+            "Entries": "Entries",
+            "Moderation": "Moderation",
+            "Playlists": "Playlists",
+            "Studio": "Studio",
+            "Analytics": "Analytics",
+            "Settings": "Settings",
+            "Administration": "Administration"
+        },
+        "userSettings":{
+            "changeAccount": "Change Acoount",
+            "logout": "Logout"
+        },
+        "helpMenu":{
+            "gettingStarted": "Getting Started",
+            "kmcOverview": "KMC Overview",
+            "mediaManagement": "Media Management & Workflow",
+            "productTour": "Watch the product tour",
+            "support": "Help and Support",
+            "userManual": "Download user manual",
+            "contact": "Contact Support",
+            "currentPage": "Current Page"
+        },
+        "languageMenu":{
+            "selectLang": "Select Language"
+        }
+    },
+    "applications":{
+        "dashboard":{
+            "main_screen_welcome_title":"{{0}}, Welcome to the Kaltura Management Console. You are logged in as a {{1}} user in the {{2}} account."
+        },
+        "content":{
+            "entries":{
+
+            }
+        }
+    }
+}
+
