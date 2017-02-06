c1d9fd5b4118a8ea72e5c419b1a7d35696b1c4d7
amirch1 <amir.chervinsky@kaltura.com>
2017-02-06 09:27:02 +0200

    remove unused scss files

--- src/theme.ts
+++ src/theme.ts
@@ -1,10 +1,3 @@
 // Import application theme (not pure vendor but we want is to bundle in vendor)
 import 'font-awesome/scss/font-awesome.scss';
 import './styles/scss/index.scss';
-
-
-/* import mixins */
-import './applications/content-entries-app/styles/scss/module-globals.scss';
-import './app/styles/scss/module-globals.scss';
-
-
