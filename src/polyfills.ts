56fe7bd817a4f7bcb7c925184ca05a11c11b7d70
Eran Sakal <eran.sakal@kaltura.com>
2017-02-08 22:30:44 +0200

    remove reflect-metadata

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,8 +1,8 @@
-import 'tslib';
+// Note: in this file, try using either commonjs or es2015 module imports (the currently selected approach is commonjs)
+require('tslib');
 
-require('core-js/shim');
-import 'reflect-metadata';
-require('zone.js/dist/zone');
+require ('core-js/shim');
+require ('zone.js/dist/zone');
 
 if (process.env.ENV === 'build') {
   // Production
