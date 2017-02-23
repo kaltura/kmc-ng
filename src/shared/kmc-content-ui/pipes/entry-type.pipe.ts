d3b2a72bd1780f4f9ad7cf15a3e978715d638332
Amir Chervinsky <amir.chervinsky@kaltura.com>
2017-02-23 08:36:18 +0200

    KMCNG-114 provide tooltip to media type column (#105)

--- src/shared/kmc-content-ui/pipes/entry-type.pipe.ts
+++ src/shared/kmc-content-ui/pipes/entry-type.pipe.ts
@@ -1,32 +1,47 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { KalturaMediaType } from '@kaltura-ng2/kaltura-api';
+import { AppLocalization } from '@kaltura-ng2/kaltura-common';
 
 @Pipe({name: 'entryType'})
 export class EntryTypePipe implements PipeTransform {
-  transform(value): string {
-    let className = 'k-entry-';
-    if (typeof(value) !== 'undefined' && value !== null)  {
-      switch (value) {
-        case KalturaMediaType.Video:
-          className += 'media';
-          break;
-        case KalturaMediaType.Image:
-          className += 'image';
-          break;
-        case KalturaMediaType.Audio:
-          className += 'audio';
-          break;
-        case KalturaMediaType.LiveStreamFlash:
-        case KalturaMediaType.LiveStreamQuicktime:
-        case KalturaMediaType.LiveStreamRealMedia:
-        case KalturaMediaType.LiveStreamWindowsMedia:
-          className += 'live';
-          break;
-        default:
-          className += 'unknown';
-          break;
-      }
-    }
-    return className;
-  }
+
+	constructor(private appLocalization: AppLocalization) {
+	}
+
+	transform(value, isTooltip: boolean): string {
+		let className = 'k-entry-';
+		let tooltip = "";
+		if (typeof(value) !== 'undefined' && value !== null) {
+			switch (value) {
+				case KalturaMediaType.Video:
+					className += 'media';
+					tooltip = this.appLocalization.get("applications.content.entryType.video");
+					break;
+				case KalturaMediaType.Image:
+					tooltip = this.appLocalization.get("applications.content.entryType.image");
+					className += 'image';
+					break;
+				case KalturaMediaType.Audio:
+					tooltip = this.appLocalization.get("applications.content.entryType.audio");
+					className += 'audio';
+					break;
+				case KalturaMediaType.LiveStreamFlash:
+				case KalturaMediaType.LiveStreamQuicktime:
+				case KalturaMediaType.LiveStreamRealMedia:
+				case KalturaMediaType.LiveStreamWindowsMedia:
+					tooltip = this.appLocalization.get("applications.content.entryType.live");
+					className += 'live';
+					break;
+				default:
+					tooltip = this.appLocalization.get("applications.content.entryType.unknown");
+					className += 'unknown';
+					break;
+			}
+		}
+		if (isTooltip) {
+			return tooltip;
+		}else {
+			return className;
+		}
+	}
 }
