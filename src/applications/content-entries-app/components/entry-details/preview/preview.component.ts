7198e3e2d488d5765abf42b7284965215c485c52
amirch1 <amir.chervinsky@kaltura.com>
2017-02-28 10:08:16 +0200

    start adding entry properties

--- src/applications/content-entries-app/components/entry-details/preview/preview.component.ts
+++ src/applications/content-entries-app/components/entry-details/preview/preview.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 import { AppConfig, AppAuthentication } from '@kaltura-ng2/kaltura-common';
-import { KalturaBaseEntry } from '@kaltura-ng2/kaltura-api/types';
+import { KalturaBaseEntry, KalturaEntryStatus } from '@kaltura-ng2/kaltura-api/types';
 
 @Component({
     selector: 'kEntryPreview',
@@ -11,8 +11,18 @@ import { KalturaBaseEntry } from '@kaltura-ng2/kaltura-api/types';
 export class PreviewComponent implements OnInit {
 
 	@Input() entryId: string;
-	@Input() currentEntry: KalturaBaseEntry;
-	iFrameSrc: string;
+
+	entryReady: boolean = false;
+	private _currentEntry: KalturaBaseEntry;
+
+	@Input() set currentEntry(entry: KalturaBaseEntry) {
+		this._currentEntry = entry;
+		this.entryReady = this._currentEntry ? this._currentEntry.status !== KalturaEntryStatus.NoContent : false;
+	}
+	get currentEntry(): KalturaBaseEntry { return this._currentEntry; }
+
+	public _iFrameSrc: string;
+	//public _landingPage: string;
 
     constructor(private appConfig: AppConfig, private appAuthentication: AppAuthentication) {
     }
@@ -20,10 +30,13 @@ export class PreviewComponent implements OnInit {
     ngOnInit() {
 	    const UIConfID = this.appConfig.get('core.kaltura.previewUIConf');
 	    const partnerID = this.appAuthentication.appUser.partnerId;
-	    this.iFrameSrc = this.appConfig.get('core.kaltura.cdnUrl') + '/p/' + partnerID +'/sp/' + partnerID +'00/embedIframeJs/uiconf_id/' + UIConfID + '/partner_id/' + partnerID +'?iframeembed=true&flashvars[EmbedPlayer.SimulateMobile]=true&&flashvars[EmbedPlayer.EnableMobileSkin]=true&entry_id='+ this.entryId;
+	    this._iFrameSrc = this.appConfig.get('core.kaltura.cdnUrl') + '/p/' + partnerID +'/sp/' + partnerID +'00/embedIframeJs/uiconf_id/' + UIConfID + '/partner_id/' + partnerID +'?iframeembed=true&flashvars[EmbedPlayer.SimulateMobile]=true&&flashvars[EmbedPlayer.EnableMobileSkin]=true&entry_id='+ this.entryId;
+		//this._landingPage = this.appAuthentication.appUser.partnerInfo.landingPage;
     }
 
-
+	openPreviewAndEmbed(){
+		alert("Open Preview & Embed Window");
+	}
 
 }
 
