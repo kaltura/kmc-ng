c569f4df699a69d315dd2c79b2d36c823aa004d5
Eran Sakal <eran.sakal@kaltura.com>
2017-04-20 21:13:24 +0300

    saving custom metadata

--- src/applications/content-entries-app/entry/entry-metadata/entry-metadata-handler.ts
+++ src/applications/content-entries-app/entry/entry-metadata/entry-metadata-handler.ts
@@ -13,9 +13,9 @@ import { MetadataProfile, MetadataProfileStore, MetadataProfileTypes, MetadataPr
 import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 import { EntrySectionsManager } from '../../entry-store/entry-sections-manager';
 import { KalturaMultiRequest } from '@kaltura-ng2/kaltura-api';
-import { DynamicFormService, SectionFormControl } from '@kaltura-ng2/kaltura-ui/dynamic-form';
 import { KalturaCustomMetadata } from '@kaltura-ng2/kaltura-ui/dynamic-form/kaltura-custom-metadata';
 import { MetadataListAction, KalturaMetadataFilter, KalturaMetadata, MetadataUpdateAction } from '@kaltura-ng2/kaltura-api/types';
+import { KalturaCustomDataHandler } from '@kaltura-ng2/kaltura-ui/dynamic-form/kaltura-custom-metadata';
 
 @Injectable()
 export class EntryMetadataHandler extends EntrySection
@@ -28,9 +28,7 @@ export class EntryMetadataHandler extends EntrySection
     private _entryMetadataStatus : 'loading' | 'loaded' | Error = null;
 
     public metadataForm : FormGroup;
-    public customMetadataForm : FormGroup;
-    public customMetadata : MetadataProfile;
-    public customMetdataGroupControl : SectionFormControl;
+    public customDataForms : KalturaCustomDataHandler;
     public loading = false;
     public loadingError = null;
 
@@ -40,7 +38,6 @@ export class EntryMetadataHandler extends EntrySection
                 private _categoriesStore : CategoriesStore,
                 private _formBuilder : FormBuilder,
                 private _kalturaCustomMetadata : KalturaCustomMetadata,
-                private _dynamicFormService: DynamicFormService,
                 private _metadataProfileStore : MetadataProfileStore)
     {
         super(manager);
@@ -108,17 +105,8 @@ export class EntryMetadataHandler extends EntrySection
                 }
             );
 
-            const metadataFormExtractValue = this._kalturaCustomMetadata.toFormValue(this._entryMetadata[0], this.customMetadata);
-
-            if (metadataFormExtractValue.error) {
-                this.loadingError = {
-                    message: metadataFormExtractValue.error.message,
-                };
-            } else
-            {
-                this.customMetadataForm.reset(metadataFormExtractValue.value);
-            }
-
+            console.warn('KMCng: map the right entry metadata to the relevant custom data form');
+            this.customDataForms.syncValue(this._entryMetadata[0]);
         } else {
             const error = (this._entryCategoriesStatus instanceof Error ? this._entryCategoriesStatus : null)
                 || (this._entryMetadataStatus instanceof Error ? this._entryMetadataStatus : null)
@@ -219,32 +207,8 @@ export class EntryMetadataHandler extends EntrySection
                 .monitor('load metadata profiles')
                 .subscribe(
                     response => {
-                        try {
-                            // get a list of controls.
-                            const customMetadataGroupControl = this._kalturaCustomMetadata.toMetadataFormSection(response.items[0]);
-
-                            if (customMetadataGroupControl) {
-                                const customMetadataFormGroup = this._dynamicFormService.toFormGroup(
-                                    [customMetadataGroupControl]
-                                );
-
-                                if (customMetadataFormGroup) {
-
-                                    this.customMetadata  = response.items[0];
-                                    this.customMetdataGroupControl = customMetadataGroupControl;
-                                    // the metadata service returns an inner group control named 'metadata' (aligned with the value).
-                                    // we should pass that actual form to our component
-                                    this.customMetadataForm = <FormGroup>customMetadataFormGroup.controls['metadata'];
-                                }
-                            }
-
-                                this._profileMetadataStatus = 'loaded';
-                        }
-                        catch (e) {
-                            this._profileMetadataStatus = e;
-
-                        }
-
+                        this.customDataForms = this._kalturaCustomMetadata.createHandler(response.items[0]);
+                        this._profileMetadataStatus = 'loaded';
                         this._updateForms();
                     },
                     error => {
@@ -257,19 +221,20 @@ export class EntryMetadataHandler extends EntrySection
 
     protected _onDataSaving(data: KalturaMediaEntry, request: KalturaMultiRequest)
     {
-        if (this.customMetadataForm.dirty) {
+        if (this.customDataForms.dirty) {
             console.warn('should check if the custom metadata form values were modified');
 
-            const metadataServerValue = this._kalturaCustomMetadata.toServerValue(this.customMetadataForm.value, this.customMetadata);
+            const customDataValue = this.customDataForms.getValue();
 
-            if (metadataServerValue.error)
+            if (customDataValue.error)
             {
-                console.warn('stop process and show error');
+                console.warn('KMCng: stop process and show error');
             }else
             {
+
                 request.requests.push(new MetadataUpdateAction({
                     id: this._entryMetadata[0].id,
-                    xmlData: metadataServerValue.value
+                    xmlData: customDataValue.xml
                 }));
             }
         }
