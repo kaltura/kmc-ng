08de13601bd4b2d690728119d44446b03604cbb0
amirch1 <amir.chervinsky@kaltura.com>
2017-04-23 14:43:03 +0300

    feat: KMCNG-250 - show Live stream properties

--- src/applications/content-entries-app/entry/entry-live/entry-live-handler.ts
+++ src/applications/content-entries-app/entry/entry-live/entry-live-handler.ts
@@ -1,16 +1,31 @@
 import { Injectable, OnDestroy } from '@angular/core';
 import { EntrySection } from '../../entry-store/entry-section-handler';
-import { ISubscription } from 'rxjs/Subscription';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Message } from 'primeng/primeng';
 import { EntrySectionTypes } from '../../entry-store/entry-sections-types';
 import { KalturaServerClient } from '@kaltura-ng2/kaltura-api';
+import { KalturaSourceType, KalturaLiveStreamBitrate, KalturaLiveStreamConfiguration, KalturaConversionProfile, ConversionProfileListAction, KalturaConversionProfileFilter,
+	KalturaConversionProfileType, KalturaFilterPager, LiveStreamRegenerateStreamTokenAction, KalturaRecordStatus, KalturaLiveStreamEntry, KalturaDVRStatus } from '@kaltura-ng2/kaltura-api/types';
+import { AppLocalization } from '@kaltura-ng2/kaltura-common';
 import { EntrySectionsManager } from '../../entry-store/entry-sections-manager';
 
 @Injectable()
 export class EntryLiveHandler extends EntrySection
 {
 
-    constructor(manager : EntrySectionsManager,
-                kalturaServerClient: KalturaServerClient)
+	public _msgs: Message[] = [];
+	private _liveType: string = "";
+
+	private _conversionProfiles : BehaviorSubject<{ items : KalturaConversionProfile[], loading : boolean, error? : any}> =
+		new BehaviorSubject<{ items : KalturaConversionProfile[], loading : boolean, error? : any}>({ items : [], loading : false});
+	public _conversionProfiles$ = this._conversionProfiles.asObservable().monitor('conversion profiles');
+
+	public _regeneratingToken: boolean = false;
+	public _recordStatus: string = "";
+	public _DVRStatus: string = "";
+	public _showDVRWindow: boolean = false;
+
+    constructor(manager : EntrySectionsManager, private _kalturaServerClient: KalturaServerClient, private _appLocalization: AppLocalization)
     {
         super(manager);
     }
@@ -28,6 +43,99 @@ export class EntryLiveHandler extends EntrySection
     }
 
     protected _activate(firstLoad : boolean) {
-        return undefined;
+	    // set live type
+	    switch (this.data.sourceType.toString()){
+		    case KalturaSourceType.liveStream.toString():
+		    	this._liveType = "kaltura";
+			    this._fetchConversionProfiles();
+			    this._setRecordStatus();
+			    this._setDVRStatus();
+			    break;
+		    case KalturaSourceType.akamaiUniversalLive.toString():
+		    	this._liveType = "universal";
+			    this._showDVRWindow = true;
+			    this._setDVRStatus();
+			    break;
+		    case KalturaSourceType.manualLiveStream.toString():
+		    	this._liveType = "manual";
+			    break;
+	    }
+    }
+
+    private _fetchConversionProfiles():void{
+	    this._conversionProfiles.next({items : [], loading : true});
+
+	    this._kalturaServerClient.request(new ConversionProfileListAction({
+		    filter: new KalturaConversionProfileFilter({
+			    typeEqual : KalturaConversionProfileType.liveStream
+		    }),
+		    pager: new KalturaFilterPager({
+			    pageIndex: 1,
+			    pageSize: 500
+		    })
+	    }))
+	    .cancelOnDestroy(this,this.sectionReset$)
+	    .monitor('get conversion profiles')
+	    .subscribe(
+		    response =>
+		    {
+			    if (response.objects && response.objects.length){
+				    // set the default profile first in the array
+				    response.objects.sort(function(a,b) {
+					    if ( a.isDefault > b.isDefault )
+						    return -1;
+					    if ( a.isDefault < b.isDefault )
+						    return 1;
+					    return 0;
+				    } );
+				    this._conversionProfiles.next({items : response.objects, loading : false});
+			    }
+
+		    },
+		    error =>
+		    {
+			    this._conversionProfiles.next({items : [], loading : false, error : error});
+		    }
+	    );
+    }
+
+    public regenerateStreamToken(): void{
+	    this._regeneratingToken = true;
+	    this._kalturaServerClient.request(new LiveStreamRegenerateStreamTokenAction({entryId: this.data.id}))
+		    .cancelOnDestroy(this,this.sectionReset$)
+		    .monitor('regenerate stream token')
+		    .subscribe(
+			    response =>
+			    {
+				    this._regeneratingToken = false;
+				    this._msgs.push({severity: 'success', summary: '', detail: this._appLocalization.get('applications.content.entryDetails.live.regenerateSuccess')});
+			    },
+			    error =>
+			    {
+				    this._regeneratingToken = false;
+				    this._msgs.push({severity: 'error', summary: '', detail: this._appLocalization.get('applications.content.entryDetails.live.regenerateFailure')});
+			    }
+		    );
+    }
+
+    private _setDVRStatus():void{
+	    let entry = this.data as KalturaLiveStreamEntry;
+	    if (!entry.dvrStatus || entry.dvrStatus.toString() === KalturaDVRStatus.disabled.toString()) {
+		    this._DVRStatus = this._appLocalization.get('app.common.off');
+	    } else if (entry.dvrStatus.toString() == KalturaDVRStatus.enabled.toString()) {
+		    this._DVRStatus = this._appLocalization.get('app.common.on');
+		    if (this._liveType === "kaltura"){
+		        this._showDVRWindow = true;
+		    }
+	    }
+    }
+
+    private _setRecordStatus(): void{
+	    let entry = this.data as KalturaLiveStreamEntry;
+	    if (!entry.recordStatus || entry.recordStatus.toString() === KalturaRecordStatus.disabled.toString()) {
+		    this._recordStatus = this._appLocalization.get('app.common.off');
+	    } else if (entry.recordStatus.toString() === KalturaRecordStatus.appended.toString() || entry.recordStatus.toString() === KalturaRecordStatus.perSession.toString()) {
+		    this._recordStatus = this._appLocalization.get('app.common.on');
+	    }
     }
 }
--- src/shared/kmc-shell/providers/browser.service.ts
+++ src/shared/kmc-shell/providers/browser.service.ts
@@ -6,44 +6,84 @@ import { IAppStorage } from '@kaltura-ng2/kaltura-common';
 @Injectable()
 export class BrowserService implements IAppStorage {
 
-  constructor(private localStorage :LocalStorageService, private sessionStorage : SessionStorageService )
-  {}
+	constructor(private localStorage: LocalStorageService, private sessionStorage: SessionStorageService) {
+	}
 
-  public setInLocalStorage(key : string, value : any) : void{
-   this.localStorage.store(key,value);
-  }
+	public setInLocalStorage(key: string, value: any): void {
+		this.localStorage.store(key, value);
+	}
 
-  public getFromLocalStorage(key : string) : any{
-    return this.localStorage.retrieve(key);
-  }
+	public getFromLocalStorage(key: string): any {
+		return this.localStorage.retrieve(key);
+	}
 
-  public removeFromLocalStorage(key : string) : any{
-    this.localStorage.clear(key);
-  }
+	public removeFromLocalStorage(key: string): any {
+		this.localStorage.clear(key);
+	}
 
-  public setInSessionStorage(key : string, value : any) : void{
-    this.sessionStorage.store(key,value);
-  }
+	public setInSessionStorage(key: string, value: any): void {
+		this.sessionStorage.store(key, value);
+	}
 
-  public getFromSessionStorage(key : string) : any{
-    return this.sessionStorage.retrieve(key);
-  }
+	public getFromSessionStorage(key: string): any {
+		return this.sessionStorage.retrieve(key);
+	}
 
-  public removeFromSessionStorage(key : string) : any{
-    this.sessionStorage.clear(key);
-  }
+	public removeFromSessionStorage(key: string): any {
+		this.sessionStorage.clear(key);
+	}
 
-  public openLink(baseUrl:string, params:any = {}, target: string = "_blank"){
-    // if we got params, append to the base URL using query string
-    if (baseUrl && baseUrl.length){
-      if (Object.keys(params).length > 0){
-        baseUrl += "?";
-        for (var key of Object.keys(params)) {
-          baseUrl += key + "=" + params[key] + "&";
-        }
-        baseUrl = baseUrl.slice(0, - 1); // remove last &
-      }
-    }
-    window.open(baseUrl, target);
-  }
+	public openLink(baseUrl: string, params: any = {}, target: string = "_blank") {
+		// if we got params, append to the base URL using query string
+		if (baseUrl && baseUrl.length) {
+			if (Object.keys(params).length > 0) {
+				baseUrl += "?";
+				for (var key of Object.keys(params)) {
+					baseUrl += key + "=" + params[key] + "&";
+				}
+				baseUrl = baseUrl.slice(0, -1); // remove last &
+			}
+		}
+		window.open(baseUrl, target);
+	}
+
+
+	public copyToClipboardEnabled(): boolean {
+		let enabled = true;
+		// detect Safari version lower than 10
+		let isChrome = !!window['chrome'] && !!window['chrome'].webstore;
+		let isSafari = Object.prototype.toString.call(window['HTMLElement']).indexOf('Constructor') > 0 || !isChrome && window['webkitAudioContext'] !== undefined;
+		if (isSafari) {
+			let nAgt = navigator.userAgent;
+			let verOffset = nAgt.indexOf("Version");
+			let fullVersion = nAgt.substring(verOffset + 8);
+			let ix;
+			if ((ix = fullVersion.indexOf(";")) != -1) {
+				fullVersion = fullVersion.substring(0, ix);
+			}
+			if ((ix = fullVersion.indexOf(" ")) != -1) {
+				fullVersion = fullVersion.substring(0, ix);
+			}
+			let majorVersion = parseInt('' + fullVersion, 10);
+			enabled = majorVersion < 10;
+		}
+		return enabled;
+	}
+
+	public copyToClipboard(text: string): boolean {
+		let copied = false;
+		let textArea = document.createElement("textarea");
+		textArea.style.position = 'fixed';
+		textArea.style.top = -1000 + 'px';
+		textArea.value = text;
+		document.body.appendChild(textArea);
+		textArea.select();
+		try {
+			copied = document.execCommand('copy');
+		} catch (err) {
+			console.log('Copy to clipboard operation failed');
+		}
+		document.body.removeChild(textArea);
+		return copied;
+	}
 }
