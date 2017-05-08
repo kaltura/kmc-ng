21f436ca445f066359e894253d7ce4f2949937b6
amirch1 <amir.chervinsky@kaltura.com>
2017-05-08 15:16:16 +0300

    feat: KMCNG-187 - set users tab

--- src/applications/content-entries-app/entry/entry-users/entry-users-handler.ts
+++ src/applications/content-entries-app/entry/entry-users/entry-users-handler.ts
@@ -1,32 +1,148 @@
 import { Injectable } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 import { EntrySection } from '../../entry-store/entry-section-handler';
 import { ISubscription } from 'rxjs/Subscription';
+import { Observable } from 'rxjs/Observable';
 import { EntrySectionTypes } from '../../entry-store/entry-sections-types';
-import { KalturaServerClient } from '@kaltura-ng2/kaltura-api';
+import { KalturaServerClient, KalturaMultiRequest } from '@kaltura-ng2/kaltura-api';
+import { KalturaUser, UserGetAction, UserListAction, KalturaUserFilter, KalturaFilterPager, KalturaMediaEntry } from '@kaltura-ng2/kaltura-api/types';
 import { EntrySectionsManager } from '../../entry-store/entry-sections-manager';
 
 @Injectable()
 export class EntryUsersHandler extends EntrySection
 {
-    constructor(manager : EntrySectionsManager,
-                kalturaServerClient: KalturaServerClient)
+    public _creator: string = "";
+	public _owner: KalturaUser = null;
+
+	public usersForm : FormGroup;
+
+	constructor(manager : EntrySectionsManager, private _formBuilder : FormBuilder, private _kalturaServerClient: KalturaServerClient)
     {
         super(manager);
+	    this._buildForm();
     }
+	private _buildForm() : void{
+		this.usersForm = this._formBuilder.group({
+			owners : null,
+			editors: null,
+			publishers: null
+		});
+
+		this.usersForm.statusChanges
+			.cancelOnDestroy(this)
+			.monitor('status changes')
+			.subscribe(
+				value =>
+				{
+					super._onStatusChanged({isValid : value === 'VALID'});
+				}
+			)
+
+	}
+
 
     public get sectionType() : EntrySectionTypes
     {
         return EntrySectionTypes.Users;
     }
 
+	protected _onDataSaving(data: KalturaMediaEntry, request: KalturaMultiRequest){
+		if (this.usersForm.dirty){
+			// save owner
+			if (this._owner && this._owner.id) {
+				data.userId = this._owner.id;
+			}
+			// save editors
+			const editors: KalturaUser[] = this.usersForm.value.editors;
+			// save publishers
+			const publishers: KalturaUser[] = this.usersForm.value.publishers;
+		}
+	}
     /**
      * Do some cleanups if needed once the section is removed
      */
     protected _reset()
     {
+	    this._creator = "";
+	    this._owner = null;
+	    this.usersForm.setValue({
+		    owners: null,
+		    editors: null,
+		    publishers: null
+	    });
     }
 
     protected _activate(firstLoad : boolean) {
-        return undefined;
+        this._fetchUsersData();
     }
+
+    private _fetchUsersData():void{
+	    const request = new KalturaMultiRequest(
+		    new UserGetAction({userId: this.data.creatorId}),
+		    new UserGetAction({userId: this.data.userId})
+	    );
+
+	    this._kalturaServerClient.multiRequest(request)
+		    .cancelOnDestroy(this,this.sectionReset$)
+		    .monitor('get users details')
+		    .subscribe(
+			    response =>
+			    {
+				    if (response.length && response.length ===2 && response[0].result){
+					    this._creator = response[0].result.screenName ? response[0].result.screenName : response[0].result.id;
+					    if (response[1].result) {
+						    this._owner = <KalturaUser>response[1].result;
+					    }
+				    }
+
+			    },
+			    error =>
+			    {
+				    console.warn("Error getting users data");
+			    }
+		    );
+    }
+
+	public searchUsers(text : string)
+	{
+		return Observable.create(
+			observer => {
+				const requestSubscription: ISubscription = this._kalturaServerClient.request(
+					new UserListAction(
+						{
+							filter: new KalturaUserFilter({
+								idOrScreenNameStartsWith : text
+							}),
+							pager: new KalturaFilterPager({
+								pageIndex : 0,
+								pageSize : 30
+							})
+						}
+					)
+				)
+				.cancelOnDestroy(this, this.sectionReset$)
+				.monitor('search owners')
+				.subscribe(
+					result =>
+					{
+						observer.next(result.objects);
+					},
+					err =>
+					{
+						observer.error(err);
+					}
+				);
+
+				return () =>
+				{
+					console.log("entryUsersHandler.searchOwners(): cancelled");
+					requestSubscription.unsubscribe();
+				}
+			});
+	}
+
+	public saveOwner(): void{
+		this._owner = this.usersForm.value.owners[0];
+	}
+
 }
--- src/applications/content-entries-app/entry/entry-users/entry-users.component.ts
+++ src/applications/content-entries-app/entry/entry-users/entry-users.component.ts
@@ -1,6 +1,10 @@
 import { Component, Input, Output, EventEmitter, ViewChild, AfterViewInit,OnInit, OnDestroy } from '@angular/core';
 import { ISubscription } from 'rxjs/Subscription';
-import { AppLocalization } from '@kaltura-ng2/kaltura-common';
+import { Subject } from 'rxjs/Subject';
+import { KalturaUser } from '@kaltura-ng2/kaltura-api/types';
+import { SuggestionsProviderData } from '@kaltura-ng2/kaltura-primeng-ui/auto-complete';
+import { PopupWidgetComponent } from '@kaltura-ng2/kaltura-ui/popup-widget/popup-widget.component';
+import { EntryUsersHandler } from './entry-users-handler';
 
 @Component({
   selector: 'kEntryUsers',
@@ -12,7 +16,12 @@ export class EntryUsers implements AfterViewInit, OnInit, OnDestroy {
     public _loading = false;
     public _loadingError = null;
 
-    constructor(private _appLocalization: AppLocalization) {
+	@ViewChild('ownerPopup') ownerPopup: PopupWidgetComponent;
+
+	private _searchUsersSubscription : ISubscription;
+	public _usersProvider = new Subject<SuggestionsProviderData>();
+
+    constructor(public _handler : EntryUsersHandler) {
     }
 
 
@@ -31,5 +40,48 @@ export class EntryUsers implements AfterViewInit, OnInit, OnDestroy {
         if (actionKey === 'retry') {
         }
     }
+
+    public _openChangeOwner(): void{
+	    this._handler.usersForm.patchValue({owners: null});
+	    this.ownerPopup.open();
+    }
+
+    public _saveAndClose(): void{
+	    this._handler.saveOwner();
+	    this.ownerPopup.close();
+    }
+
+	public _searchUsers(event, formControl) : void {
+		this._usersProvider.next({ suggestions : [], isLoading : true});
+
+		if (this._searchUsersSubscription)
+		{
+			// abort previous request
+			this._searchUsersSubscription.unsubscribe();
+			this._searchUsersSubscription = null;
+		}
+
+		this._searchUsersSubscription = this._handler.searchUsers(event.query).subscribe(data => {
+				const suggestions = [];
+				(data || []).forEach((suggestedUser: KalturaUser) => {
+					let isSelectable = true;
+					if (formControl){
+						const owners = this._handler.usersForm.value[formControl] || [];
+						isSelectable = !owners.find(user => {
+							return user.id === suggestedUser.id;
+						});
+					}
+					suggestions.push({
+						name: suggestedUser.screenName + "(" + suggestedUser.id + ")",
+						item: suggestedUser,
+						isSelectable: isSelectable
+					});
+				});
+				this._usersProvider.next({suggestions: suggestions, isLoading: false});
+			},
+			(err) => {
+				this._usersProvider.next({ suggestions : [], isLoading : false, errorMessage : <any>(err.message || err)});
+			});
+	}
 }
 
