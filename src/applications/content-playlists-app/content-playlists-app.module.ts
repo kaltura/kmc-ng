c668e199268dec1f49256a9e617db4088b9f35eb
Konstantin Stepanets <konstantin.stepanets@gmail.com>
2017-06-06 16:44:12 +0300

    KMCNG-48 were removed the temporary unnecessary modules from content-playlists-app.module.ts

--- src/applications/content-playlists-app/content-playlists-app.module.ts
+++ src/applications/content-playlists-app/content-playlists-app.module.ts
@@ -1,77 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
-import { TagsModule } from '@kaltura-ng2/kaltura-ui/tags';
-import {
-     TreeModule, TieredMenuModule,  SharedModule,   AccordionModule,  ButtonModule, InputTextareaModule, PaginatorModule, InputTextModule, MenuModule, DataTableModule, DropdownModule, RadioButtonModule, MultiSelectModule, CheckboxModule, CalendarModule, SpinnerModule, ConfirmDialogModule, ConfirmationService, GrowlModule } from 'primeng/primeng';
-import { KMCShellModule } from 'kmc-shell';
-import { TreeSelectionModule } from '@kaltura-ng2/kaltura-primeng-ui/tree-selection';
 
 import { routing } from './content-playlists-app.routes';
 import { ContentPlaylistsComponent } from './content-playlists.component';
 
-import { MetadataProfileStore } from '@kaltura-ng2/kaltura-common';
-
-import { KalturaCommonModule } from '@kaltura-ng2/kaltura-common';
-import { KalturaPrimeNgUIModule } from '@kaltura-ng2/kaltura-primeng-ui';
-import { KalturaUIModule, TooltipModule } from '@kaltura-ng2/kaltura-ui';
-import { AutoCompleteModule } from '@kaltura-ng2/kaltura-primeng-ui/auto-complete';
-import { PopupWidgetModule } from '@kaltura-ng2/kaltura-ui/popup-widget';
-
-import { AreaBlockerModule } from '@kaltura-ng2/kaltura-ui';
-import { DynamicFormModule } from '@kaltura-ng2/kaltura-ui/dynamic-form';
-import { DynamicFormModule as PrimeDynamicFormModule } from '@kaltura-ng2/kaltura-primeng-ui/dynamic-form';
-import { KalturaCustomMetadataModule } from '@kaltura-ng2/kaltura-ui/dynamic-form/kaltura-custom-metadata';
-
-
 @NgModule({
     imports: [
-        AccordionModule,
-        AreaBlockerModule,
-        AutoCompleteModule,
-        ButtonModule,
-        CalendarModule,
-        CheckboxModule,
         CommonModule,
-        ConfirmDialogModule,
-        DataTableModule,
-        DropdownModule,
-        DynamicFormModule,
-        FormsModule,
-        GrowlModule,
-        InputTextareaModule,
-        InputTextModule,
-        KalturaCommonModule,
-        KalturaCustomMetadataModule,
-        KalturaPrimeNgUIModule,
-        KalturaUIModule,
-        KMCShellModule,
-        MenuModule,
-        MultiSelectModule,
-        PaginatorModule,
-        PopupWidgetModule,
-        PrimeDynamicFormModule,
-        RadioButtonModule,
-        ReactiveFormsModule,
-        RouterModule.forChild(routing),
-        SharedModule,
-        SpinnerModule,
-        TagsModule,
-        TieredMenuModule,
-        TooltipModule,
-        TreeModule,
-        TreeSelectionModule,
+        RouterModule.forChild(routing)
     ],
     declarations: [
 		ContentPlaylistsComponent
     ],
     exports: [],
-    providers: [
-        ConfirmationService,
-        MetadataProfileStore
-    ],
+    providers: [],
 })
 export class ContentPlaylistsAppModule {
 }
