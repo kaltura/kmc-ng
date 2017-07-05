c722819a820f37385f269848168a4b45f9bdd4fb
Konstantin Stepanets <k.stepanets@synergetica.net>
2017-07-05 15:02:11 +0300

    was added kPrimeTableSortDirection pipe to set the appropriate initial sort direction

--- src/applications/content-playlists-app/playlists/playlists-components-list.ts
+++ src/applications/content-playlists-app/playlists/playlists-components-list.ts
@@ -2,10 +2,12 @@ import { PlaylistsListComponent } from "./playlists-list.component";
 import { PlaylistsTableComponent } from './playlists-table.component';
 import { PlaylistTypePipe } from './pipes/playlist-type.pipe';
 import { PlaylistsAdditionalFiltersComponent } from "./playlists-additional-filters/playlists-additional-filters.component";
+import { PrimeTableSortDirectionPipe } from './pipes/prime-table-sort-direction.pipe';
 
 export const PlaylistsComponentsList = [
-    PlaylistsListComponent,
-	PlaylistsTableComponent,
-	PlaylistsAdditionalFiltersComponent,
-	PlaylistTypePipe
+  PlaylistsListComponent,
+  PlaylistsTableComponent,
+  PlaylistsAdditionalFiltersComponent,
+  PlaylistTypePipe,
+  PrimeTableSortDirectionPipe
 ];
