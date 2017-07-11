f04a9418f5c0f1d730f5af860c395f0d2f40b3ed
Konstantin Stepanets <k.stepanets@synergetica.net>
2017-07-11 18:53:23 +0300

    Playlist-content. Entries table. (1-st part pf updates)

---
+++ src/applications/content-playlists-app/playlist/playlist-entries-table/playlist-entries-table.component.ts
@@ -0,0 +1,60 @@
+import { Component, Input, Output,	EventEmitter,	AfterViewInit, OnInit, OnDestroy,	ViewChild, ChangeDetectorRef } from '@angular/core';
+import { ISubscription } from 'rxjs/Subscription';
+import { MenuItem, DataTable, Menu } from 'primeng/primeng';
+import { AppLocalization } from '@kaltura-ng/kaltura-common';
+import { AreaBlockerMessage } from '@kaltura-ng/kaltura-ui';
+import { KalturaMediaType } from 'kaltura-typescript-client/types/KalturaMediaType';
+import { KalturaMediaEntry } from 'kaltura-typescript-client/types/KalturaMediaEntry';
+import { KalturaEntryStatus } from 'kaltura-typescript-client/types/KalturaEntryStatus';
+import { PlaylistStore } from "../playlist-store.service";
+
+@Component({
+	selector: 'kPlaylistEntriesTable',
+	templateUrl: './playlist-entries-table.component.html',
+	styleUrls: ['./playlist-entries-table.component.scss']
+})
+export class PlaylistEntriesTableComponent implements AfterViewInit, OnInit, OnDestroy {
+	public _entries: any[] = [];
+  private _deferredEntries : any[];
+  public _deferredLoading = true;
+	public rowTrackBy: Function = (index: number, item: any) => {return item.id};
+
+  @Input() set entries(data: any[]) {
+    if (!this._deferredLoading) {
+      this._entries = [];
+      this.cdRef.detectChanges();
+      this._entries = data;
+      this.cdRef.detectChanges();
+    } else {
+      this._deferredEntries = data
+    }
+  }
+
+	constructor(
+		private appLocalization: AppLocalization,
+		public playlistStore: PlaylistStore,
+		private cdRef: ChangeDetectorRef
+	) {}
+	ngOnInit() {
+    /*this.playlistStore.entries$
+      .subscribe(response => {
+
+      });*/
+	}
+
+	ngAfterViewInit() {
+    if (this._deferredLoading) {
+      // use timeout to allow the DOM to render before setting the data to the datagrid. This prevents the screen from hanging during datagrid rendering of the data.
+      setTimeout(()=> {
+        this._deferredLoading = false;
+        this._entries = this._deferredEntries;
+        this._deferredEntries = null;
+      }, 0);
+    }
+	}
+
+	ngOnDestroy() {
+
+	}
+}
+
