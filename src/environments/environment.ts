0e97a0c25cf98f9e8de6e3d0ecc6ad18ed7ed077
Konstantin Stepanets <k.stepanets@synergetica.net>
2017-07-14 19:24:51 +0300

    Deleting from action + from bulk

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -112,7 +112,8 @@ export const environment = {
       "bulkActionsLimit": 2
     },
     "contentPlaylists" : {
-      "createdAtDateRange" : "2005:2030"
+      "createdAtDateRange" : "2005:2030",
+      "bulkActionsLimit": 2
     }
   },
   "entriesShared": {
