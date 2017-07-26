eb37e6626bf777bab713b6bd8a125c3b204ae093
Konstantin Stepanets <konstantin.stepanets@gmail.com>
2017-07-26 14:57:56 +0300

    feat: playlist navigation between sections and warn when leaving before changes were saved

--- src/applications/content-playlists-app/content-playlists-app.routes.ts
+++ src/applications/content-playlists-app/content-playlists-app.routes.ts
@@ -5,16 +5,17 @@ import { PlaylistComponent } from './playlist/playlist.component';
 import { PlaylistMetadataComponent } from './playlist/playlist-metadata/playlist-metadata.component';
 import { PlaylistContentComponent } from './playlist/playlist-content/playlist-content.component';
 import { PlaylistSections } from './playlist/playlist-sections';
+import { PlaylistCanDeactivate } from './playlist/playlist-can-deactivate.service';
 
 export const routing: Route[] = [
 	{path: '', redirectTo: 'list', pathMatch: 'full'},
 	{path: 'list', component: PlaylistsListComponent},
-	{path: 'playlist/:id', component: PlaylistComponent,
+	{path: 'playlist/:id', canDeactivate: [PlaylistCanDeactivate], component: PlaylistComponent,
 		data : {
 			playlistRoute : true
 		},
 		children : [
-			{ path: '', redirectTo: 'metadata', pathMatch: 'full' },
+			{ path: '', redirectTo: 'content', pathMatch: 'full' },
 			{ path: 'metadata', component: PlaylistMetadataComponent, data : { sectionKey : PlaylistSections.Metadata } },
 			{ path: 'content', component: PlaylistContentComponent, data : { sectionKey : PlaylistSections.Content } }
 		]
