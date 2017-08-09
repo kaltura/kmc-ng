5a08716e2a2a9c7f0b392d253a6da3bd6fde3cab
konstantin <konstantin.stepanets@gmail.com>
2017-08-09 19:33:40 +0300

    KMCNG-461 Initialize new playlist

---
+++ src/applications/content-playlists-app/playlists/add-new-playlist/add-new-playlist.component.ts
@@ -0,0 +1,58 @@
+import { Component, Input, OnInit, AfterViewInit, OnDestroy } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { KalturaPlaylist } from 'kaltura-typescript-client/types/KalturaPlaylist';
+import { KalturaPlaylistType } from 'kaltura-typescript-client/types/KalturaPlaylistType';
+import { PopupWidgetComponent } from '@kaltura-ng/kaltura-ui/popup-widget/popup-widget.component';
+
+@Component({
+  selector: 'kAddNewPlaylist',
+  templateUrl: './add-new-playlist.component.html',
+  styleUrls: ['./add-new-playlist.component.scss']
+})
+export class AddNewPlaylist implements  OnInit, AfterViewInit, OnDestroy{
+
+  @Input() parentPopupWidget: PopupWidgetComponent;
+  addNewPlaylistFrom: FormGroup;
+  selectedValue: string = 'manual';
+  selectedCheckboxValue: string;
+  newPlaylist: KalturaPlaylist;
+
+  constructor(
+    private _formBuilder : FormBuilder,
+    public router: Router
+  ) {
+    // build FormControl group
+    this.addNewPlaylistFrom = _formBuilder.group({
+      name        : ['', Validators.required],
+      description : '',
+      playlistType: '',
+      ruleBasedSub: false
+    });
+  }
+
+  goNext() {
+    this.router.navigate(['/content/playlists/playlist/new/content'], this.newPlaylist);
+  }
+
+  ngOnInit(){
+    this.addNewPlaylistFrom.valueChanges
+      .cancelOnDestroy(this)
+      .subscribe(
+        form => {
+          let playlistType = form.playlistType === 'manual' ? KalturaPlaylistType.staticList : KalturaPlaylistType.dynamic;
+          this.newPlaylist = new KalturaPlaylist({
+            name: form.name,
+            description: form.description,
+            playlistType: playlistType
+          });
+          /* ToDo have to figure out how to send the "ruleBasedSub" value */
+        }
+      );
+  }
+
+  ngAfterViewInit(){}
+
+  ngOnDestroy(){}
+}
+
