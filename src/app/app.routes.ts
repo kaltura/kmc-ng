ee0f519cb0ff09d64b1a252f616037bdf989274a
Stas <staskgt@gmail.com>
2017-08-11 18:34:54 +0300

    feat(upload-control): add upload control module
    Add upload control module and implement basic page with table for future development

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -17,15 +17,28 @@ const routes: Routes = <Routes>[
       { path: 'login', component: LoginComponent },
       {
         path: '', component: DashboardComponent, canActivate: [AuthCanActivate], children: [
+        {
+          path: 'content', children: [
+          { path: '', redirectTo: 'entries', pathMatch: 'full' },
           {
-            path: 'content', children: [
-              { path: '', redirectTo: 'entries', pathMatch: 'full' },
-              { path: 'entries', loadChildren: '../applications/content-entries-app/content-entries-app.module#ContentEntriesAppModule' },
-              { path: 'playlists', loadChildren: '../applications/content-playlists-app/content-playlists-app.module#ContentPlaylistsAppModule' },
-              { path: 'categories', loadChildren: '../applications/content-categories-app/content-categories-app.module#ContentCategoriesAppModule' }
-            ]
-          }
+            path: 'entries',
+            loadChildren: '../applications/content-entries-app/content-entries-app.module#ContentEntriesAppModule'
+          },
+          {
+            path: 'playlists',
+            loadChildren: '../applications/content-playlists-app/content-playlists-app.module#ContentPlaylistsAppModule'
+          },
+          {
+            path: 'categories',
+            loadChildren: '../applications/content-categories-app/content-categories-app.module#ContentCategoriesAppModule'
+          },
+          {
+            path: 'upload-control',
+            loadChildren: '../applications/content-upload-control-app/content-upload-control-app.module#ContentUploadControlAppModule'
+          },
         ]
+        }
+      ]
       },
       {
         path: '**', redirectTo: '/login', pathMatch: 'full'
