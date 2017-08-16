9c4419b8651bf347da72454963f731ffedff6174
Stas <staskgt@gmail.com>
2017-08-16 18:17:29 +0300

    feat(upload-menu): add "in development" messages

--- src/applications/kmc-upload-app/upload-menu/upload-menu.component.ts
+++ src/applications/kmc-upload-app/upload-menu/upload-menu.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { UploadSettingsHandler } from '../upload-settings/upload-settings-handler';
+import { BrowserService } from 'app-shared/kmc-shell';
+import { AppLocalization } from '@kaltura-ng/kaltura-common';
 
 @Component({
   selector: 'kKMCUploadMenu',
@@ -13,6 +15,17 @@ export class UploadMenuComponent {
     return this._handler.allowedExtensions;
   }
 
-  constructor(private _handler: UploadSettingsHandler) {
+  constructor(private _handler: UploadSettingsHandler,
+              private _browserService: BrowserService,
+              private _appLocalization: AppLocalization) {
+  }
+
+  public _inDevelopment(): void {
+    this._browserService.alert(
+      {
+        header: this._appLocalization.get('applications.upload.inDevelopment.title'),
+        message: this._appLocalization.get('applications.upload.inDevelopment.message')
+      }
+    );
   }
 }
