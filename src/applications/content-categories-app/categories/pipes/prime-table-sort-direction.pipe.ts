20d3eac1fd2600622e47d374c6a6f324667bdfac
KALTURA\anat.grossberg <anat.grossberg@kaltura.com>
2017-08-20 11:54:21 +0300

    change Sort to 'custom'

---
+++ src/applications/content-categories-app/categories/pipes/prime-table-sort-direction.pipe.ts
@@ -0,0 +1,21 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { AppLocalization } from '@kaltura-ng/kaltura-common';
+import { SortDirection } from "../../categories/categories.service";
+
+
+@Pipe({name: 'kPrimeTableSortDirection'})
+export class PrimeTableSortDirectionPipe implements PipeTransform {
+	constructor(private appLocalization: AppLocalization) {
+	}
+
+	transform(value: SortDirection,): number {
+
+    switch (value)
+	{
+		case SortDirection.Asc:
+			return 1;
+		case SortDirection.Desc:
+			return -1;
+	}
+  }
+}
