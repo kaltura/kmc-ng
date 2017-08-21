2a32ca22c41137c1699cfda4bd1b4aae186442d6
Anat123 <grossberg.anat@gmail.com>
2017-08-21 14:05:13 +0300

    feat(content): support categories list actions like reload, pagination and sorting

--- src/applications/content-categories-app/categories/categories-components-list.ts
+++ src/applications/content-categories-app/categories/categories-components-list.ts
@@ -2,10 +2,13 @@ import { CategoriesTableComponent } from './categories-table.component';
 import { CategoriesListComponent } from "./categories-list.component";
 import { MaxCategoriesPipe } from './pipes/max-categories.pipe';
 import { CategoriesBulkActionsComponent } from './bulk-actions/categories-bulk-actions.component';
+import { PrimeTableSortDirectionPipe } from './pipes/prime-table-sort-direction.pipe';
+
 
 export const CategoriesComponentsList = [
     CategoriesListComponent,
     CategoriesTableComponent,
     MaxCategoriesPipe,
-    CategoriesBulkActionsComponent
+    CategoriesBulkActionsComponent,
+    PrimeTableSortDirectionPipe
 ];
