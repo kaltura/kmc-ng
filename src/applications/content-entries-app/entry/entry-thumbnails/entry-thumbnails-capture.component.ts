73d23c8ca5514034f24fc4c2327d473d1788b7e9
amirch1 <amir.chervinsky@kaltura.com>
2017-08-24 09:58:45 +0300

    add thumbnail capture component files

---
+++ src/applications/content-entries-app/entry/entry-thumbnails/entry-thumbnails-capture.component.ts
@@ -0,0 +1,19 @@
+import { Component,AfterViewInit } from '@angular/core';
+import { PopupWidgetComponent, PopupWidgetStates } from '@kaltura-ng/kaltura-ui/popup-widget/popup-widget.component';
+
+@Component({
+    selector: 'kThumbnailCapture',
+    templateUrl: './entry-thumbnail-capture.component.html',
+    styleUrls: ['./entry-thumbnail-capture.component.scss']
+})
+export class EntryThumbnailCapture implements AfterViewInit{
+
+    constructor() {
+    }
+
+
+	ngAfterViewInit(){
+
+	}
+}
+
