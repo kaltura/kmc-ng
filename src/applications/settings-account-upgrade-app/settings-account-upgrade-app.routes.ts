df3219bc5e74c6621ba7140d334d187bcfa3d074
benstov <benstov@gmail.com>
2017-08-29 11:40:38 +0300

    account-settings (#189)
    feat: settings module - account settings and account upgrade forms

---
+++ src/applications/settings-account-upgrade-app/settings-account-upgrade-app.routes.ts
@@ -0,0 +1,6 @@
+import {Route} from '@angular/router';
+import {SettingsAccountUpgradeComponent} from './settings-account-upgrade.component';
+
+export const routing: Route[] = [
+  {path: '', component: SettingsAccountUpgradeComponent}
+];
