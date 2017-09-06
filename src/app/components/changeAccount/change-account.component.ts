4cbc20128e4a09016d0f6a913ec7b3ef62c04ce3
ben.simantov <ben.simantov@kaltura.com>
2017-09-06 17:14:11 +0300

    added partners loading

--- src/app/components/changeAccount/change-account.component.ts
+++ src/app/components/changeAccount/change-account.component.ts
@@ -2,25 +2,66 @@ import {Component, Input, OnInit, AfterViewInit, OnDestroy, ViewChild} from '@an
 import {FormGroup, FormBuilder} from '@angular/forms';
 import {ISubscription} from 'rxjs/Subscription';
 import {AppLocalization} from '@kaltura-ng/kaltura-common';
-import {BrowserService} from 'app-shared/kmc-shell';
+import {AppAuthentication, BrowserService} from 'app-shared/kmc-shell';
 import {PopupWidgetComponent, PopupWidgetStates} from '@kaltura-ng/kaltura-ui/popup-widget/popup-widget.component';
+import {KalturaClient} from '@kaltura-ng/kaltura-client';
+import {PartnerListPartnersForUserAction} from 'kaltura-typescript-client/types/PartnerListPartnersForUserAction';
+import {KalturaPartnerFilter} from 'kaltura-typescript-client/types/KalturaPartnerFilter';
+import {KalturaPartnerStatus} from 'kaltura-typescript-client/types/KalturaPartnerStatus';
+import {Observable} from 'rxjs/Observable';
+import {AreaBlockerMessage} from '@kaltura-ng/kaltura-ui';
 
 @Component({
   selector: 'kChangeAccount',
   templateUrl: './change-account.component.html',
   styleUrls: ['./change-account.component.scss']
 })
-export class ChangeAccountComponent implements OnInit{
+export class ChangeAccountComponent implements OnInit {
 
   @Input() parentPopupWidget: PopupWidgetComponent;
 
-
+  public partners: { 'id': number, 'name': string }[];
   public changeAccountForm: FormGroup;
-  constructor(private _fb: FormBuilder) {
+  public _blockerMessage: AreaBlockerMessage = null;
+  public _isBusy = false;
+  public _currentPartnerId: number;
+
+  constructor(private _fb: FormBuilder,
+              private _appLocalization: AppLocalization,
+              private _kalturaServerClient: KalturaClient,
+              userAuthentication: AppAuthentication) {
+   this._currentPartnerId = userAuthentication.appUser.partnerId
   }
 
   ngOnInit() {
     this._createForm();
+    this.loadAvailablePartners();
+  }
+
+  private loadAvailablePartners() {
+    this._isBusy = true;
+    this.getAvailablePartners()
+      .subscribe(partners => {
+          this.partners = partners;
+          console.log(partners);
+          this._isBusy = false;
+          this._blockerMessage = null;
+        },
+        error => {
+          this._blockerMessage = new AreaBlockerMessage(
+            {
+              message: this._appLocalization.get('app.changeAccount.errors.loadPartners'),
+              buttons: [
+                {
+                  label: this._appLocalization.get('app.common.retry'),
+                  action: () => {
+                    this.loadAvailablePartners();
+                  }
+                }
+              ]
+            }
+          );
+        });
   }
 
   public _saveAndClose(): void {
@@ -31,8 +72,21 @@ export class ChangeAccountComponent implements OnInit{
 
   private _createForm(): void {
     this.changeAccountForm = this._fb.group({
-      account: 'upload',
+      account: this._currentPartnerId,
     });
+  }
+
+  private getAvailablePartners(): Observable<{ 'id': number, 'name': string }[]> {
+    const filter = new KalturaPartnerFilter({
+      statusEqual: KalturaPartnerStatus.active
+    });
+
+    return this._kalturaServerClient.request(new PartnerListPartnersForUserAction({
+      partnerFilter: filter
+    }))
+      .map(data => {
+        return data.objects.map(partner => ({'id': partner.id, 'name': partner.name}))
+      });
   }
 }
 
