7a3950c417e0d591de8ca7910a1cf966121b8762
Stas <staskgt@gmail.com>
2017-09-12 16:58:46 +0300

    refactor: remove redundant files

--- src/applications/kmc-bulk-upload-app/kmc-bulk-upload-app.component.ts
+++ src/applications/kmc-bulk-upload-app/kmc-bulk-upload-app.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'kBulkUploadApp',
-  templateUrl: './kmc-bulk-upload-app.component.html',
-  styleUrls: ['./kmc-bulk-upload-app.component.scss'],
+  template: '<router-outlet></router-outlet>',
 })
 
 export class KmcBulkUploadAppComponent {
--- src/applications/kmc-bulk-upload-app/kmc-bulk-upload-app.module.ts
+++ src/applications/kmc-bulk-upload-app/kmc-bulk-upload-app.module.ts
@@ -1,19 +1,16 @@
-import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { routing } from './kmc-bulk-upload-app.routes';
-import { BulkUploadButtonComponent } from './bulk-upload-button/bulk-upload-button.component';
-import { BulkUploadMenuComponent } from './bulk-upload-menu/bulk-upload-menu.component';
 import { CommonModule } from '@angular/common';
 import { PopupWidgetModule } from '@kaltura-ng/kaltura-ui/popup-widget';
 import { KalturaCommonModule } from '@kaltura-ng/kaltura-common';
 import { KalturaServerModule } from '@kaltura-ng/kaltura-server-utils';
 import { ButtonModule } from 'primeng/primeng';
 import { KalturaUIModule } from '@kaltura-ng/kaltura-ui';
+import { BulkUploadMenuComponent } from './bulk-upload-menu/bulk-upload-menu.component';
+import { BulkUploadButtonComponent } from './bulk-upload-button/bulk-upload-button.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forChild(routing),
     PopupWidgetModule,
     KalturaCommonModule,
     KalturaServerModule,
