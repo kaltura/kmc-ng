37c5801c6b0432acf9613e4217fb98fae46bf2b4
Stas <staskgt@gmail.com>
2017-09-14 12:19:36 +0300

    refactor: move log type determining logic to util function

--- src/applications/content-bulk-log-app/pipes/bulk-log-type-icon.pipe.ts
+++ src/applications/content-bulk-log-app/pipes/bulk-log-type-icon.pipe.ts
@@ -1,23 +1,10 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { KalturaBulkUploadType } from 'kaltura-typescript-client/types/KalturaBulkUploadType';
+import { getBulkUploadType } from '../utils/get-bulk-upload-type';
 
 @Pipe({ name: 'kBulkLogTableTypeIcon' })
 export class BulkLogTypeIconPipe implements PipeTransform {
   transform(value: KalturaBulkUploadType): string {
-    switch (true) {
-      case KalturaBulkUploadType.csv.equals(value):
-        return 'csv';
-
-      case KalturaBulkUploadType.xml.equals(value):
-      case KalturaBulkUploadType.dropFolderXml.equals(value):
-        return 'xml';
-
-      case KalturaBulkUploadType.ical.equals(value):
-      case KalturaBulkUploadType.dropFolderIcal.equals(value):
-        return 'ics';
-
-      default:
-        return 'txt';
-    }
+    return getBulkUploadType(value);
   }
 }
