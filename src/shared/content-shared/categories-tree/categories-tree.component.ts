e5851aa72690e794a14ef02cd039d8a60995c97c
Stas <staskgt@gmail.com>
2017-09-25 17:46:40 +0300

    feat: allow set single selection on categories-tree compoenent

--- src/shared/content-shared/categories-tree/categories-tree.component.ts
+++ src/shared/content-shared/categories-tree/categories-tree.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges, ViewChild } from '@angular/core';
 import { PrimeTreeNode } from '@kaltura-ng/kaltura-primeng-ui';
 import { AppAuthentication } from 'app-shared/kmc-shell';
 import { AppLocalization } from '@kaltura-ng/kaltura-common';
@@ -6,19 +6,29 @@ import { AreaBlockerMessage } from '@kaltura-ng/kaltura-ui';
 import { PrimeTreePropagation } from '@kaltura-ng/kaltura-primeng-ui/prime-tree';
 import { CategoriesPrimeService } from 'app-shared/content-shared/categories-prime.service';
 
+export enum TreeSelectionMode {
+  Checkbox,
+  Radiobutton
+}
+
 @Component({
   selector: 'k-categories-tree',
   templateUrl: './categories-tree.component.html',
   styleUrls: ['./categories-tree.component.scss']
 })
-export class CategoriesTreeComponent implements OnInit {
+export class CategoriesTreeComponent implements OnInit, OnChanges {
 
   @Input() public disablePropagation = false;
   @Input() autoLoad = true;
-  @Input() selection: PrimeTreeNode[];
+  @Input() selection: PrimeTreeNode | PrimeTreeNode[];
+
+  @Input()
+  set selectionMode(value: TreeSelectionMode) {
+    this._selectionMode = value === TreeSelectionMode.Radiobutton ? value : TreeSelectionMode.Checkbox;
+  }
 
   @Output() onCategoriesLoad = new EventEmitter<{ categories: PrimeTreeNode[] }>();
-  @Output() selectionChange = new EventEmitter<PrimeTreeNode[]>();
+  @Output() selectionChange = new EventEmitter<PrimeTreeNode[] | PrimeTreeNode>();
   @Output() onNodeChildrenLoaded = new EventEmitter<{ node: PrimeTreeNode }>();
   @Output() onNodeSelect: EventEmitter<any> = new EventEmitter();
   @Output() onNodeUnselect: EventEmitter<any> = new EventEmitter();
@@ -28,35 +38,19 @@ export class CategoriesTreeComponent implements OnInit {
   private inLazyMode = false;
   public _loading = false;
   public _blockerMessage: AreaBlockerMessage = null;
+  public _selectionMode: TreeSelectionMode = TreeSelectionMode.Checkbox;
+  public _selectionModes = TreeSelectionMode;
 
   public _categories: PrimeTreeNode[] = [];
+
+  public _singleSelectedValue: string | number;
 
   public updateNodeState(node: PrimeTreeNode, addToSelection: boolean): void {
     this._primeTreeNodesState.updateNodeState(node, addToSelection);
   }
 
-  get categories(): PrimeTreeNode[] {
+  public get categories(): PrimeTreeNode[] {
     return this._categories;
-  }
-
-  public _selectionChange(selection: PrimeTreeNode[]) {
-    this.selection = selection;
-    this.selectionChange.emit(selection);
-  }
-
-  public findNodeByFullIdPath(fullIdPath: (number | string)[]): PrimeTreeNode {
-    // find the item in the tree (if exists)
-    let result: PrimeTreeNode = null;
-    for (let i = 0, length = fullIdPath.length; i < length; i++) {
-      const itemIdToSearchFor = fullIdPath[i];
-      result = ((result ? result.children : this._categories) || []).find(child => child.data + '' === itemIdToSearchFor + '');
-
-      if (!result) {
-        break;
-      }
-    }
-
-    return result;
   }
 
   constructor(private _categoriesPrimeService: CategoriesPrimeService,
@@ -68,11 +62,26 @@ export class CategoriesTreeComponent implements OnInit {
     this.inLazyMode = this._appAuthentication.appUser.permissionsFlags.indexOf('DYNAMIC_FLAG_KMC_CHUNKED_CATEGORY_LOAD') !== -1;
 
     if (this.autoLoad) {
-      this.loadCategories();
+      this._loadCategories();
     }
   }
 
-  loadCategories(): void {
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.selection && this._selectionMode === TreeSelectionMode.Radiobutton) {
+      const selection: PrimeTreeNode = Array.isArray(this.selection) ? this.selection[0] : this.selection;
+      this._singleSelectedValue = selection ? selection.data : null;
+    }
+  }
+  public _selectionChange(selection: PrimeTreeNode | PrimeTreeNode[]): void {
+    if (this._selectionMode === TreeSelectionMode.Radiobutton) {
+      this._singleSelectedValue = selection ? (<PrimeTreeNode>selection).data : null;
+    }
+
+    this.selection = selection;
+    this.selectionChange.emit(selection);
+  }
+
+  private _loadCategories(): void {
     this._loading = true;
     this._blockerMessage = null;
     this._categoriesPrimeService.getCategories()
@@ -86,7 +95,7 @@ export class CategoriesTreeComponent implements OnInit {
             message: error.message || this._appLocalization.get('applications.content.entryDetails.errors.categoriesLoadError'),
             buttons: [{
               label: this._appLocalization.get('applications.content.entryDetails.errors.retry'),
-              action: () => this.loadCategories()
+              action: () => this._loadCategories()
             }]
           });
           this._loading = false;
@@ -105,10 +114,34 @@ export class CategoriesTreeComponent implements OnInit {
     }
   }
 
-  public _blockTreeSelection(e: MouseEvent) {
+  public _blockTreeSelection(e: MouseEvent): void {
     e.preventDefault();
     e.stopPropagation();
   }
 
+  public findNodeByFullIdPath(fullIdPath: (number | string)[]): PrimeTreeNode {
+    // find the item in the tree (if exists)
+    let result: PrimeTreeNode = null;
+    for (let i = 0, length = fullIdPath.length; i < length; i++) {
+      const itemIdToSearchFor = fullIdPath[i];
+      result = ((result ? result.children : this._categories) || []).find(child => child.data + '' === itemIdToSearchFor + '');
+
+      if (!result) {
+        break;
+      }
+    }
+
+    return result;
+  }
+
+  public clearSelection(): void {
+    let resetValue = [];
+
+    if (this._selectionMode === TreeSelectionMode.Radiobutton) {
+      resetValue = null;
+    }
+
+    this._selectionChange(resetValue);
+  }
 }
 
