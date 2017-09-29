e905dd5e022a7e1a7d2c4f32336e3e9a76564693
Stas <staskgt@gmail.com>
2017-09-29 18:04:29 +0300

    feat: show changelog once for user, change icons

--- src/app/components/changelog/changelog.component.ts
+++ src/app/components/changelog/changelog.component.ts
@@ -1,14 +1,33 @@
-import { Component, Output, ViewChild, EventEmitter, Input } from '@angular/core';
-import {BrowserService} from 'app-shared/kmc-shell';
-import {AppAuthentication, AppUser, PartnerPackageTypes, AppNavigator} from 'app-shared/kmc-shell';
-import {environment} from 'app-environment';
-import {Md5} from 'ts-md5/dist/md5';
-import {PopupWidgetComponent} from '@kaltura-ng/kaltura-ui/popup-widget/popup-widget.component';
+import { Component, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { BrowserService } from 'app-shared/kmc-shell';
+import { PopupWidgetComponent } from '@kaltura-ng/kaltura-ui/popup-widget/popup-widget.component';
 
 @Component({
   selector: 'kChangelog',
   templateUrl: './changelog.component.html',
   styleUrls: ['./changelog.component.scss']
 })
-export class ChangelogComponent {
+export class ChangelogComponent implements OnInit {
+  @Output() onChangelogSeen = new EventEmitter<boolean>();
+  @ViewChild('changelog') changelogPopup: PopupWidgetComponent;
+
+  private _changelogSeen = false;
+  private _changelogCacheToken = 'kmc-changelog-seen';
+
+  constructor(private _browserService: BrowserService) {
+  }
+
+  ngOnInit() {
+    this._changelogSeen = !!this._browserService.getFromLocalStorage(this._changelogCacheToken);
+    setTimeout(() => {
+      this.onChangelogSeen.emit(this._changelogSeen);
+    });
+  }
+
+  public _openChangelog(): void {
+    this._changelogSeen = true;
+    this._browserService.setInLocalStorage(this._changelogCacheToken, this._changelogSeen);
+    this.onChangelogSeen.emit(this._changelogSeen);
+    this.changelogPopup.open();
+  }
 }
