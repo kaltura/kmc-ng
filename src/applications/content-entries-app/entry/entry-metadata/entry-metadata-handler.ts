d206da4fd283753fc7fc698dbcc5c3910072448b
Stas <staskgt@gmail.com>
2017-10-12 12:47:09 +0300

    fix: get relevant form metadata after forms were updated

--- src/applications/content-entries-app/entry/entry-metadata/entry-metadata-handler.ts
+++ src/applications/content-entries-app/entry/entry-metadata/entry-metadata-handler.ts
@@ -86,6 +86,7 @@ export class EntryMetadataHandler extends EntryFormWidget
 
         Observable.merge(...formsChanges)
             .cancelOnDestroy(this, this.widgetReset$)
+            .delay(0) // must defer to the next event cycle loop to get valid form metadata
             .subscribe(
                 () => {
                     let isValid = true;
