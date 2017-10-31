02d43b0d9f366675a86492866c9f09953d255cc4
Eran Sakal <eran@sakalim.com>
2017-10-31 15:40:32 +0200

    feat: prevent getting cached translation file when upgrading to new version

--- src/app-config/index.ts
+++ src/app-config/index.ts
@@ -1,7 +1,6 @@
 export const environment = {
     "appVersion": "3.1.0", // will be changed by release script
     "shell": {
-        "languageHash": "12", /* this value is set manually at the moment and should be replaced with the published app version  */
         "defaultRoute": "/content/entries",
         "loginRoute" : "/login",
         "errorRoute" : "/error",
