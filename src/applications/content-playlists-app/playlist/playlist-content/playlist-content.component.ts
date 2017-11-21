082179a0bab02aee5492b52078232ddd2b9d8d72
Stas <staskgt@gmail.com>
2017-11-21 13:52:29 +0200

    refactor: remove confirmation popup on delete

--- src/applications/content-playlists-app/playlist/playlist-content/playlist-content.component.ts
+++ src/applications/content-playlists-app/playlist/playlist-content/playlist-content.component.ts
@@ -2,8 +2,6 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { PlaylistStore } from '../playlist-store.service';
 import { KalturaMediaEntry } from 'kaltura-typescript-client/types/KalturaMediaEntry';
 import { PlaylistContentWidget } from './playlist-content-widget.service';
-import { BrowserService } from 'app-shared/kmc-shell';
-import { AppLocalization } from '@kaltura-ng/kaltura-common';
 
 @Component({
   selector: 'kPlaylistContent',
@@ -14,9 +12,7 @@ export class PlaylistContentComponent implements OnInit, OnDestroy {
   public _selectedEntries: KalturaMediaEntry[] = [];
 
   constructor(public _playlistStore: PlaylistStore,
-              public _widgetService: PlaylistContentWidget,
-              private _browserService: BrowserService,
-              private _appLocalization: AppLocalization) {
+              public _widgetService: PlaylistContentWidget) {
   }
 
   ngOnInit() {
@@ -29,30 +25,6 @@ export class PlaylistContentComponent implements OnInit, OnDestroy {
 
   public _clearSelection() {
     this._selectedEntries = [];
-  }
-
-  public _deleteEntries(selectedEntries: KalturaMediaEntry[]) {
-    const entriesToDelete = selectedEntries.map((entry, index) => `${index + 1}: ${entry.name}`);
-    const entries = selectedEntries.length <= 10 ? `${entriesToDelete.join(',').replace(/,/gi, '\n')}\n` : '';
-    const plural = selectedEntries.length > 1;
-    const header = this._appLocalization.get(`applications.content.playlists.${plural ? 'deleteEntries' : 'deleteEntry'}`);
-    const message = `
-      ${this._appLocalization.get('applications.content.playlists.' + (plural ? 'confirmDeleteEntries' : 'confirmDeleteEntry'))}\n
-      ${entries}
-      ${this._appLocalization.get('applications.content.playlists.' + (plural ? 'deleteEntriesNote' : 'deleteEntryNote'))}
-    `;
-    this._browserService.confirm(
-      {
-        header,
-        message,
-        accept: () => {
-          setTimeout(() => {
-            this._widgetService.deleteSelectedEntries(selectedEntries);
-            this._clearSelection();
-          }, 0);
-        }
-      }
-    );
   }
 
   public _onActionSelected(event: { action: string, entry: KalturaMediaEntry }): void {
