7b23aa34274f613fbaeeb858d3b3f46850c954b4
Stas <staskgt@gmail.com>
2017-11-29 18:29:29 +0200

    WIP

--- src/applications/kmc-upload-app/upload-monitor/bulk-upload-monitor.service.ts
+++ src/applications/kmc-upload-app/upload-monitor/bulk-upload-monitor.service.ts
@@ -13,11 +13,53 @@ import { BrowserService } from 'app-shared/kmc-shell';
 import { KalturaDetachedResponseProfile } from 'kaltura-ngx-client/api/types/KalturaDetachedResponseProfile';
 import { KalturaResponseProfileType } from 'kaltura-ngx-client/api/types/KalturaResponseProfileType';
 import { KalturaBulkUpload } from 'kaltura-ngx-client/api/types/KalturaBulkUpload';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { UploadMonitorStatuses } from './upload-monitor.component';
+import { KalturaBulkUploadObjectType } from 'kaltura-ngx-client/api/types/KalturaBulkUploadObjectType';
+
+export class BulkLogUploadChanges implements RequestFactory<BulkListAction> {
+  private _uploadedOn: Date;
+
+  public set uploadedOn(value: number | Date) {
+    if (value) {
+      this._uploadedOn = typeof value === 'number' ? new Date(value) : value;
+    }
+  }
+
+  constructor() {
+  }
+
+  create(): BulkListAction {
+    const bulkUploadObjectTypeIn = [
+      KalturaBulkUploadObjectType.entry,
+      KalturaBulkUploadObjectType.category,
+      KalturaBulkUploadObjectType.user,
+      KalturaBulkUploadObjectType.categoryUser
+    ];
+    return new BulkListAction({
+      bulkUploadFilter: new KalturaBulkUploadFilter({
+        bulkUploadObjectTypeIn: bulkUploadObjectTypeIn.join(','),
+        uploadedOnGreaterThanOrEqual: this._uploadedOn
+      }),
+      responseProfile: new KalturaDetachedResponseProfile({
+        type: KalturaResponseProfileType.includeFields,
+        fields: 'id,status,uploadedOn'
+      })
+    });
+  }
+}
 
 @Injectable()
 export class BulkUploadMonitorService implements OnDestroy {
   private _bulkUploadFiles: { [key: string]: { status: KalturaBatchJobStatus, uploadedOn: Date, id: number } } = {};
+  private _totals = new BehaviorSubject<UploadMonitorStatuses>({ uploading: 0, queued: 0, completed: 0, errors: 0 });
   private _bulkUploadChangesFactory = new BulkLogUploadChanges();
+  private _bulkUploadObjectTypeIn = [
+    KalturaBulkUploadObjectType.entry,
+    KalturaBulkUploadObjectType.category,
+    KalturaBulkUploadObjectType.user,
+    KalturaBulkUploadObjectType.categoryUser
+  ];
   private _activeStatuses = [
     KalturaBatchJobStatus.dontProcess,
     KalturaBatchJobStatus.pending,
@@ -36,40 +78,55 @@ export class BulkUploadMonitorService implements OnDestroy {
     KalturaBatchJobStatus.movefile
   ];
 
+  public totals$ = this._totals.asObservable();
+
   constructor(private _kalturaClient: KalturaClient,
               private _serverPolls: KalturaServerPolls,
               private _appEvents: AppEventsService,
               private _browserService: BrowserService) {
+    this._appEvents
+      .event(BulkLogUploadingStartedEvent)
+      .cancelOnDestroy(this)
+      .subscribe(({ id, status, uploadedOn }) => {
+        this._bulkUploadFiles[id] = { id, status, uploadedOn };
+        this._totals.next(this._calculateTotalsFromState());
+      });
   }
 
   ngOnDestroy() {
-
+    this._totals.complete();
   }
 
-  private _calculateTotals(): { uploading: number, queued: number, completed: number, errors: number } {
+  private _calculateTotalsFromState(): UploadMonitorStatuses {
     return Object.keys(this._bulkUploadFiles).reduce((totals, key) => {
       const upload = this._bulkUploadFiles[key];
       switch (upload.status) {
         case KalturaBatchJobStatus.pending:
         case KalturaBatchJobStatus.queued:
         case KalturaBatchJobStatus.dontProcess:
-          return Object.assign(totals, { queued: totals.queued + 1 });
+          totals.queued += 1;
+          break;
         case KalturaBatchJobStatus.processing:
         case KalturaBatchJobStatus.almostDone:
         case KalturaBatchJobStatus.retry:
-          return Object.assign(totals, { uploading: totals.uploading + 1 });
+          totals.uploading += 1;
+          break;
         case KalturaBatchJobStatus.finished:
         case KalturaBatchJobStatus.finishedPartially:
         case KalturaBatchJobStatus.processed:
-          return Object.assign(totals, { completed: totals.completed + 1 });
+          totals.completed += 1;
+          break;
         case KalturaBatchJobStatus.failed:
         case KalturaBatchJobStatus.fatal:
         case KalturaBatchJobStatus.aborted:
         case KalturaBatchJobStatus.movefile:
-          return Object.assign(totals, { errors: totals.errors + 1 });
+          totals.errors += 1;
+          break;
         default:
-          return totals;
+          break;
       }
+
+      return totals;
     }, { uploading: 0, queued: 0, completed: 0, errors: 0 });
   }
 
@@ -84,7 +141,7 @@ export class BulkUploadMonitorService implements OnDestroy {
     const activeUploads = new BulkListAction({
       bulkUploadFilter: new KalturaBulkUploadFilter({
         statusIn: processingStatusId.join(','),
-        bulkUploadObjectTypeIn: '1,2,3,4',
+        bulkUploadObjectTypeIn: this._bulkUploadObjectTypeIn.join(','),
       }),
       responseProfile: new KalturaDetachedResponseProfile({
         type: KalturaResponseProfileType.includeFields,
@@ -104,32 +161,45 @@ export class BulkUploadMonitorService implements OnDestroy {
     })
   }
 
-  public getTotals(): Observable<{ uploading: number, queued: number, completed: number, errors: number }> {
-    const mainFlow$ = this._getActiveUploadsList()
-      .do(response => {
-        response.objects.forEach(upload => {
-          this._bulkUploadFiles[upload.id] = {
-            id: upload.id,
-            status: upload.status,
-            uploadedOn: upload.uploadedOn
-          }
-        });
-      })
-      .map(response =>
-        response.objects.length
-          ? Math.min(...response.objects.map(({ uploadedOn }) => Number(uploadedOn)))
-          : this._browserService.sessionStartedAt)
-      .switchMap(smallestUploadedOn => {
-        this._bulkUploadChangesFactory.uploadedOn = smallestUploadedOn;
-        return this._serverPolls.register(10, this._bulkUploadChangesFactory)
-      })
-      .do(([response]) => {
+  private _prepare(): void {
+    this._getActiveUploadsList()
+      .subscribe(
+        response => {
+          response.objects.forEach(upload => {
+            this._bulkUploadFiles[upload.id] = {
+              id: upload.id,
+              status: upload.status,
+              uploadedOn: upload.uploadedOn
+            }
+          });
+
+          this._bulkUploadChangesFactory.uploadedOn = response.objects.length
+            ? Math.min(...response.objects.map(({ uploadedOn }) => Number(uploadedOn)))
+            : this._browserService.sessionStartedAt;
+        },
+        error => {
+          this._totals.error(error);
+        }
+      );
+  }
+
+  private _startPolling(): void {
+    this._serverPolls.register(10, this._bulkUploadChangesFactory)
+      .subscribe(([response]) => {
+        if (response.error) {
+          this._totals.error(response.error);
+          return;
+        }
+
+        const uploads = response.result.objects;
         let smallestUploadedOn;
         let needUpdateFactory = false;
 
-        this._cleanUpDeletedUploads(response.result.objects);
+        this._cleanUpDeletedUploads(uploads);
 
-        response.result.objects.forEach(upload => {
+        uploads.forEach(upload => {
+
+          // TODO for tomorrow: fix this one
           if (this._finishedStatuses.indexOf(upload.status) !== -1) {
             needUpdateFactory = true;
             smallestUploadedOn = Number(upload.uploadedOn) < smallestUploadedOn ? upload.uploadedOn : smallestUploadedOn;
@@ -137,7 +207,8 @@ export class BulkUploadMonitorService implements OnDestroy {
 
           let relevantUpload = this._bulkUploadFiles[upload.id];
           if (!relevantUpload) {
-            if (this._activeStatuses.indexOf(upload.status) !== -1) {
+            const currentUploadIsActive = this._activeStatuses.indexOf(upload.status) !== -1;
+            if (currentUploadIsActive) {
               relevantUpload = { id: upload.id, status: upload.status, uploadedOn: upload.uploadedOn };
               this._bulkUploadFiles[upload.id] = relevantUpload;
             }
@@ -149,41 +220,8 @@ export class BulkUploadMonitorService implements OnDestroy {
         if (needUpdateFactory) {
           this._bulkUploadChangesFactory.uploadedOn = smallestUploadedOn;
         }
+
+        this._totals.next(this._calculateTotalsFromState());
       });
-
-    const newUploadFlow$ = this._appEvents
-      .event(BulkLogUploadingStartedEvent)
-      .do(({ id, status, uploadedOn }) => {
-        this._bulkUploadFiles[id] = { id, status, uploadedOn };
-      });
-
-    return Observable.merge(mainFlow$, newUploadFlow$)
-      .map(() => this._calculateTotals());
-  }
-}
-
-export class BulkLogUploadChanges implements RequestFactory<BulkListAction> {
-  private _uploadedOn: Date;
-
-  public set uploadedOn(value: number | Date) {
-    if (value) {
-      this._uploadedOn = typeof value === 'number' ? new Date(value) : value;
-    }
-  }
-
-  constructor() {
-  }
-
-  create(): BulkListAction {
-    return new BulkListAction({
-      bulkUploadFilter: new KalturaBulkUploadFilter({
-        bulkUploadObjectTypeIn: '1,2,3,4',
-        uploadedOnGreaterThanOrEqual: this._uploadedOn
-      }),
-      responseProfile: new KalturaDetachedResponseProfile({
-        type: KalturaResponseProfileType.includeFields,
-        fields: 'id,status,uploadedOn'
-      })
-    });
   }
 }
