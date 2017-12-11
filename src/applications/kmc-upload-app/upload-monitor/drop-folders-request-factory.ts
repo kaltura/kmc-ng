4e70ffee292a79a9de785aa5b2cbf339521f0c76
Stas <staskgt@gmail.com>
2017-12-11 19:29:31 +0200

    add drop-folders monitor support

---
+++ src/applications/kmc-upload-app/upload-monitor/drop-folders-request-factory.ts
@@ -0,0 +1,31 @@
+import { KalturaDetachedResponseProfile } from 'kaltura-ngx-client/api/types/KalturaDetachedResponseProfile';
+import { KalturaResponseProfileType } from 'kaltura-ngx-client/api/types/KalturaResponseProfileType';
+import { RequestFactory } from '@kaltura-ng/kaltura-common';
+import { DropFolderFileListAction } from 'kaltura-ngx-client/api/types/DropFolderFileListAction';
+import { KalturaDropFolderFileListResponse } from 'kaltura-ngx-client/api/types/KalturaDropFolderFileListResponse';
+import { KalturaDropFolderFileFilter } from 'kaltura-ngx-client/api/types/KalturaDropFolderFileFilter';
+
+export class DropFoldersRequestFactory implements RequestFactory<DropFolderFileListAction, KalturaDropFolderFileListResponse> {
+  public uploadedOn: Date;
+  public dropFolderIdIn: string;
+
+  constructor() {
+  }
+
+  create(): DropFolderFileListAction {
+    if (this.uploadedOn === null || this.dropFolderIdIn === null) {
+      return null;
+    }
+
+    return new DropFolderFileListAction({
+      filter: new KalturaDropFolderFileFilter({
+        createdAtGreaterThanOrEqual: this.uploadedOn,
+        dropFolderIdIn: this.dropFolderIdIn
+      }),
+      responseProfile: new KalturaDetachedResponseProfile({
+        type: KalturaResponseProfileType.includeFields,
+        fields: 'id,status,createdAt'
+      })
+    });
+  }
+}
