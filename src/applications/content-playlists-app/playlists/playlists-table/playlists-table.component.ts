2d2388718d31fe7d8e70bf275a4fbcc93ad2d9d8
Stas <staskgt@gmail.com>
2017-12-20 14:58:25 +0200

    wip on filters refactoring

--- src/applications/content-playlists-app/playlists/playlists-table/playlists-table.component.ts
+++ src/applications/content-playlists-app/playlists/playlists-table/playlists-table.component.ts
@@ -1,11 +1,11 @@
 import { AfterViewInit, ChangeDetectorRef, Component, EventEmitter, Input, OnDestroy, OnInit, Output, ViewChild } from '@angular/core';
 import { ISubscription } from 'rxjs/Subscription';
-import { DataTable, Menu, MenuItem } from 'primeng/primeng';
-import { AppLocalization } from '@kaltura-ng/kaltura-common';
-import { AreaBlockerMessage } from '@kaltura-ng/kaltura-ui';
-import { PlaylistsStore } from './playlists-store/playlists-store.service';
-import { Filter } from './playlists-list.component';
+import { Menu, MenuItem } from 'primeng/primeng';
+import { PlaylistsStore } from '../playlists-store/playlists-store.service';
+import { Filter } from '../playlists-list/playlists-list.component';
 import { KalturaPlaylist } from 'kaltura-ngx-client/api/types/KalturaPlaylist';
+import { AreaBlockerMessage } from '@kaltura-ng/kaltura-ui/area-blocker/area-blocker-message';
+import { AppLocalization } from '@kaltura-ng/kaltura-common/localization/app-localization.service';
 
 @Component({
   selector: 'kPlaylistsTable',
@@ -13,17 +13,12 @@ import { KalturaPlaylist } from 'kaltura-ngx-client/api/types/KalturaPlaylist';
   styleUrls: ['./playlists-table.component.scss']
 })
 export class PlaylistsTableComponent implements AfterViewInit, OnInit, OnDestroy {
-  public _blockerMessage: AreaBlockerMessage = null;
-  public _playlists: any[] = [];
-  private _deferredPlaylists: any[];
-
-  @Input()
-  set playlists(data: any[]) {
+  @Input() set playlists(data: KalturaPlaylist[]) {
     if (!this._deferredLoading) {
       this._playlists = [];
-      this.cdRef.detectChanges();
+      this._cdRef.detectChanges();
       this._playlists = data;
-      this.cdRef.detectChanges();
+      this._cdRef.detectChanges();
     } else {
       this._deferredPlaylists = data
     }
@@ -32,62 +27,69 @@ export class PlaylistsTableComponent implements AfterViewInit, OnInit, OnDestroy
   @Input() filter: any = {};
   @Input() selectedPlaylists: any[] = [];
   @Input() activeFilters: Filter[] = [];
+
   @Output() sortChanged = new EventEmitter<any>();
   @Output() selectedPlaylistsChange = new EventEmitter<any>();
   @Output() actionSelected = new EventEmitter<any>();
-  @ViewChild('dataTable') private dataTable: DataTable;
+
   @ViewChild('actionsmenu') private actionsMenu: Menu;
+
+  private _deferredPlaylists: KalturaPlaylist[];
+
   private actionsMenuPlaylistId = '';
   private actionsMenuPlaylist: KalturaPlaylist;
   private playlistsStoreStatusSubscription: ISubscription;
 
   public _deferredLoading = true;
   public _emptyMessage = '';
-
+  public _blockerMessage: AreaBlockerMessage = null;
+  public _playlists: KalturaPlaylist[] = [];
   public _items: MenuItem[];
 
   public rowTrackBy: Function = (index: number, item: any) => {
     return item.id
   };
 
-  constructor(private appLocalization: AppLocalization,
+  constructor(private _appLocalization: AppLocalization,
               public playlistsStore: PlaylistsStore,
-              private cdRef: ChangeDetectorRef) {
+              private _cdRef: ChangeDetectorRef) {
   }
 
   ngOnInit() {
     this._blockerMessage = null;
     this._emptyMessage = '';
     let loadedOnce = false; // used to set the empty message to 'no results' only after search
-    this.playlistsStoreStatusSubscription = this.playlistsStore.state$.subscribe(
-      result => {
-        if (result.errorMessage) {
-          this._blockerMessage = new AreaBlockerMessage({
-            message: result.errorMessage || 'Error loading entries',
-            buttons: [{
-              label: 'Retry',
-              action: () => {
-                this.playlistsStore.reload(true);
+    this.playlistsStore.playlists.state$
+      .cancelOnDestroy(this)
+      .subscribe(
+        result => {
+          if (result.errorMessage) {
+            this._blockerMessage = new AreaBlockerMessage({
+              message: result.errorMessage || 'Error loading entries',
+              buttons: [{
+                label: this._appLocalization.get('app.common.retry'),
+                action: () => {
+                  this.playlistsStore.reload();
+                }
+              }
+              ]
+            })
+          } else {
+            this._blockerMessage = null;
+            if (result.loading) {
+              this._emptyMessage = '';
+              loadedOnce = true;
+            } else {
+              if (loadedOnce) {
+                this._emptyMessage = this._appLocalization.get('applications.content.table.noResults');
               }
             }
-            ]
-          })
-        } else {
-          this._blockerMessage = null;
-          if (result.loading) {
-            this._emptyMessage = '';
-            loadedOnce = true;
-          } else {
-            if (loadedOnce) {
-              this._emptyMessage = this.appLocalization.get('applications.content.table.noResults');
-            }
           }
-        }
-      },
-      error => {
-        console.warn('[kmcng] -> could not load playlists'); // navigate to error page
-        throw error;
-      });
+        },
+        error => {
+          console.warn('[kmcng] -> could not load playlists'); // navigate to error page
+          throw error;
+        });
   }
 
   ngAfterViewInit() {
@@ -123,15 +125,15 @@ export class PlaylistsTableComponent implements AfterViewInit, OnInit, OnDestroy
   buildMenu(): void {
     this._items = [
       {
-        label: this.appLocalization.get('applications.content.table.previewAndEmbed'),
+        label: this._appLocalization.get('applications.content.table.previewAndEmbed'),
         command: () => this.onActionSelected('preview', this.actionsMenuPlaylist)
       },
       {
-        label: this.appLocalization.get('applications.content.table.delete'),
+        label: this._appLocalization.get('applications.content.table.delete'),
         command: () => this.onActionSelected('delete', this.actionsMenuPlaylist)
       },
       {
-        label: this.appLocalization.get('applications.content.table.view'),
+        label: this._appLocalization.get('applications.content.table.view'),
         command: () => this.onActionSelected('view', this.actionsMenuPlaylist)
       }
     ];
