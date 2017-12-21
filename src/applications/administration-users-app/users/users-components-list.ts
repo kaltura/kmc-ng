2de44372d3ea6acc897d0488b26e7e97d3f18e01
Stas <staskgt@gmail.com>
2017-12-21 12:09:21 +0200

    use pipe for account owner

--- src/applications/administration-users-app/users/users-components-list.ts
+++ src/applications/administration-users-app/users/users-components-list.ts
@@ -2,10 +2,12 @@ import { UsersTableComponent } from './users-table.component';
 import { StatusPipe } from './pipes/status.pipe';
 import { RoleNamePipe } from './pipes/role-name.pipe';
 import { UsersListComponent } from './users-list.component';
+import { AccountOwnerPipe } from './pipes/account-owner.pipe';
 
 export const UsersComponentsList = [
   UsersTableComponent,
   StatusPipe,
   RoleNamePipe,
-  UsersListComponent
+  UsersListComponent,
+  AccountOwnerPipe
 ];
