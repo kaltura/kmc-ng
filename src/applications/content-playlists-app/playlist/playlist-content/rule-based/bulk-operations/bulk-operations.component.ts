0163d6aa7cbde29dca637b1f3ffdb872e5607a6e
Stas <staskgt@gmail.com>
2017-12-22 17:09:33 +0200

    fix imports

--- src/applications/content-playlists-app/playlist/playlist-content/rule-based/bulk-operations/bulk-operations.component.ts
+++ src/applications/content-playlists-app/playlist/playlist-content/rule-based/bulk-operations/bulk-operations.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { PlaylistRule } from '../rule-based-content-widget.service';
+import { PlaylistRule } from 'app-shared/content-shared/playlist-rule.interface';
 
 @Component({
   selector: 'kPlaylistRulesListBulkOperationsContent',
--- src/applications/content-playlists-app/playlist/playlist-content/rule-based/playlist-rules-table/playlist-rules-order-by.pipe.ts
+++ src/applications/content-playlists-app/playlist/playlist-content/rule-based/playlist-rules-table/playlist-rules-order-by.pipe.ts
@@ -1,7 +1,7 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { AppLocalization } from '@kaltura-ng/kaltura-common';
-import { PlaylistRule } from '../rule-based-content-widget.service';
 import { KalturaPlayableEntryOrderBy } from 'kaltura-ngx-client/api/types/KalturaPlayableEntryOrderBy';
+import { PlaylistRule } from 'app-shared/content-shared/playlist-rule.interface';
 
 @Pipe({ name: 'playlistRuleOrderBy' })
 export class PlaylistOrderByPipe implements PipeTransform {
