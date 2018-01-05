07622505a46290820737068c54b0266dd1e7de5a
Stas <staskgt@gmail.com>
2018-01-05 13:00:51 +0200

    show field value in the form

--- src/applications/settings-custom-data-app/schemas/custom-schema/custom-schema-field-form/custom-schema-field-form.component.ts
+++ src/applications/settings-custom-data-app/schemas/custom-schema/custom-schema-field-form/custom-schema-field-form.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input } from '@angular/core';
-import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, EventEmitter, Input, OnDestroy, Output } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup } from '@angular/forms';
 import { AppLocalization } from '@kaltura-ng/kaltura-common/localization/app-localization.service';
 import { MetadataItem, MetadataItemTypes } from 'app-shared/kmc-shared/custom-metadata/metadata-profile';
 
@@ -8,18 +8,22 @@ import { MetadataItem, MetadataItemTypes } from 'app-shared/kmc-shared/custom-me
   templateUrl: './custom-schema-field-form.component.html',
   styleUrls: ['./custom-schema-field-form.component.scss']
 })
-export class CustomSchemaFieldFormComponent {
+export class CustomSchemaFieldFormComponent implements OnDestroy {
   @Input() set field(value: MetadataItem | null) {
     if (value) {
-      this._field = value;
+      this._isNew = false;
+      this._field = <MetadataItem>Object.assign({}, value);
       this._title = this._appLocalization.get('applications.settings.metadata.editCustomField');
-
+      this._setInitialValue(this._field);
     } else {
       this._title = this._appLocalization.get('applications.settings.metadata.addCustomField');
     }
   }
 
+  @Output() onClose = new EventEmitter<void>();
+
   private _field: MetadataItem;
+  private _isNew = true;
 
   public _title;
   public _fieldForm: FormGroup;
@@ -58,11 +62,34 @@ export class CustomSchemaFieldFormComponent {
     this._buildForm();
   }
 
+  ngOnDestroy() {
+
+  }
+
+  private _setInitialValue(field: MetadataItem): void {
+    this._fieldForm.setValue({
+      type: field.type,
+      allowMultiple: field.allowMultiple,
+      label: field.label,
+      shortDescription: field.description,
+      description: field.documentations,
+      searchable: !!field.isSearchable,
+
+      includeTime: !!field.isTimeControl,
+      listValues: (field.optionalValues && field.optionalValues.length)
+        ? field.optionalValues.map(({ value }) => value).join('\n')
+        : ''
+    });
+
+    this._typeField.disable();
+    this._allowMultipleField.disable();
+  }
+
   private _buildForm(): void {
     this._fieldForm = this._fb.group({
       type: MetadataItemTypes.Text,
       allowMultiple: false,
-      label: ['', Validators.required],
+      label: '',
       shortDescription: '',
       description: '',
       searchable: true,
@@ -83,9 +110,28 @@ export class CustomSchemaFieldFormComponent {
     this._includeTimeField = this._fieldForm.controls['includeTime'];
     this._listValuesFiled = this._fieldForm.controls['listValues'];
 
-    this._typeField.valueChanges.subscribe(change => {
-      this._fieldForm.patchValue({ searchable: change !== MetadataItemTypes.Date });
-    });
+    this._typeField.valueChanges
+      .cancelOnDestroy(this)
+      .filter(() => this._isNew)
+      .subscribe(change => {
+        this._fieldForm.patchValue({ searchable: change !== MetadataItemTypes.Date });
+      });
+  }
+
+  private _update(): void {
+
+  }
+
+  private _create(): void {
+
+  }
+
+  public _cancel(): void {
+    this.onClose.emit();
+  }
+
+  public _save(): void {
+
   }
 }
 
