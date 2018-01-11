d4530c1064ace62c9ca3b549967ba6bc0a517241
Eran Sakal <eran@sakalim.com>
2018-01-11 14:39:37 +0200

    code cleanup

--- src/applications/content-categories-app/category/category.service.ts
+++ src/applications/content-categories-app/category/category.service.ts
@@ -19,7 +19,6 @@ import {OnDataSavingReasons} from '@kaltura-ng/kaltura-ui';
 import {BrowserService} from 'app-shared/kmc-shell/providers/browser.service';
 import {PageExitVerificationService} from 'app-shared/kmc-shell/page-exit-verification';
 import {AppEventsService} from 'app-shared/kmc-shared';
-import { CategoryDeleteAction } from 'kaltura-ngx-client/api/types/CategoryDeleteAction';
 
 export enum ActionTypes {
   CategoryLoading,
@@ -188,21 +187,6 @@ export class CategoryService implements OnDestroy {
                         .tag('block-shell')
                         .map(
 						categorySavedResponse => {
-
-						    const bla = request.requests.find((innerRequest, index) =>
-                            {
-                                return (innerRequest instanceof CategoryDeleteAction
-                                && categorySavedResponse[index].result);
-                            });
-
-						    if (bla)
-                            {
-                                // whatever
-                            }
-                            request.requests.filter(innerRequest => innerRequest instanceof CategoryDeleteAction)
-
-
-
 							if (categorySavedResponse.hasErrors()) {
 								this._state.next({ action: ActionTypes.CategorySavingFailed });
 							} else {
