e3876330a3289cc7b857ac182e811c6625f9b8fe
Stas <staskgt@gmail.com>
2018-01-19 15:18:41 +0200

    wip on validation of autocomplete input

--- src/applications/settings-access-control-app/profiles/edit-profile/edit-profile.component.ts
+++ src/applications/settings-access-control-app/profiles/edit-profile/edit-profile.component.ts
@@ -30,5 +30,29 @@ export class EditProfileComponent {
   public _save(): void {
 
   }
+
+  public _validateDomain(item: string): boolean {
+    return /^([a-zA-Z0-9]+(\.[a-zA-Z0-9]+)+.*)$/.test(item);
+  }
+
+  public _validateIp(item: string): boolean {
+    const ipRegExp = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+    if (typeof item === 'string') {
+      const cidrOrMask = item.split('/');
+      if (cidrOrMask.length === 2) { // x.x.x.x/x or x.x.x.x/m.m.m.m
+        return ipRegExp.test(cidrOrMask[0]) && (ipRegExp.test(cidrOrMask[1]) || /^\d+$/.test(cidrOrMask[1]));
+      }
+
+      const range = item.split('-');
+      if (range.length === 2) { // x.x.x.x-x.x.x.x
+        return ipRegExp.test(range[0]) && (ipRegExp.test(range[1]));
+      }
+
+      return ipRegExp.test(item); // x.x.x.x
+    }
+
+
+    return false;
+  }
 }
 
