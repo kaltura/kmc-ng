9af9c39a24b0b1c012654029f8a76436d59e98d6
Eran Sakal <eran@sakalim.com>
2018-01-28 16:59:10 +0200

    add developer comment in categories status module

--- src/shared/content-shared/categories-status/categories-status.module.ts
+++ src/shared/content-shared/categories-status/categories-status.module.ts
@@ -10,7 +10,9 @@ import { CategoriesStatusMonitorService } from './categories-status-monitor.serv
 })
 export class CategoriesStatusModule {
     constructor(@Optional() @Self() private _categoriesStatusMonitorService: CategoriesStatusMonitorService){
-
+        // NOTICE: it is important to get '_categoriesStatusMonitorService'
+        // even when it is not in use internally.
+        // getting it from the DI will force initiation of that service
     }
     static forRoot(): ModuleWithProviders {
         return {
