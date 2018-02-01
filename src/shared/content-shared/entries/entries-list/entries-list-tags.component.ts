a3126d4d9260ef389534cfad8ef4b2b16cbe01c0
Eran Sakal <eran@sakalim.com>
2018-02-01 11:00:32 +0200

    changed locale format into fixed formats

--- src/shared/content-shared/entries/entries-list/entries-list-tags.component.ts
+++ src/shared/content-shared/entries/entries-list/entries-list-tags.component.ts
@@ -197,11 +197,11 @@ export class EntriesListTagsComponent implements OnInit, OnDestroy {
         if (fromDate || toDate) {
             let tooltip = '';
             if (fromDate && toDate) {
-                tooltip = `${moment(fromDate).format('LL')} - ${moment(toDate).format('LL')}`;
+                tooltip = `${moment(fromDate).format('MMMM D, YYYY')} - ${moment(toDate).format('MMMM D, YYYY')}`;
             } else if (fromDate) {
-                tooltip = `From ${moment(fromDate).format('LL')}`;
+                tooltip = `From ${moment(fromDate).format('MMMM D, YYYY')}`;
             } else if (toDate) {
-                tooltip = `Until ${moment(toDate).format('LL')}`;
+                tooltip = `Until ${moment(toDate).format('MMMM D, YYYY')}`;
             }
 
             this._tags.push({type: 'createdAt', value: null, label: 'Dates', tooltip });
