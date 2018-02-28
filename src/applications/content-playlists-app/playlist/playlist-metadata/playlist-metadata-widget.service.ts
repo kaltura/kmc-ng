674c92f7ad17f304d009eced2cba89af0048a7e9
Stas <staskgt@gmail.com>
2018-02-28 17:37:24 +0200

    set default name for new playlist, mark as dirty

--- src/applications/content-playlists-app/playlist/playlist-metadata/playlist-metadata-widget.service.ts
+++ src/applications/content-playlists-app/playlist/playlist-metadata/playlist-metadata-widget.service.ts
@@ -76,6 +76,10 @@ export class PlaylistMetadataWidget extends PlaylistWidget implements OnDestroy 
   }
 
   protected onActivate(firstTimeActivating: boolean): void {
+    if (this.isNewData && (this.data.playlistContent || '').trim().length > 0) {
+      this.updateState({ isDirty: true });
+    }
+
     this.metadataForm.reset({
       name: this.data.name,
       description: this.data.description,
