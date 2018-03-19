e1b91a8d591b8cde9c6bba3a05b9b704ddf7c9e4
amirch1 <amir.chervinsky@kaltura.com>
2018-03-19 09:59:44 +0200

    chore(release): 4.0.1

--- src/configuration/global-config.ts
+++ src/configuration/global-config.ts
@@ -27,7 +27,7 @@ export interface GlobalConfig {
 export const globalConfig: GlobalConfig = {
     client: {
         production: environment.production,
-        appVersion: '4.0.0',
+        appVersion: '4.0.1',
         useSecuredProtocol: environment.client.useSecuredProtocol,
         countriesList: countryCodes,
         views: {
