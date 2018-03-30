5ad93f8c3f429da1bed0cf1d4825100384c70577
Stas <staskgt@gmail.com>
2018-03-30 17:26:12 +0300

    mark all new playlists dirty by default

--- src/applications/content-playlists-app/playlist/playlist-metadata/playlist-metadata-widget.service.ts
+++ src/applications/content-playlists-app/playlist/playlist-metadata/playlist-metadata-widget.service.ts
@@ -76,7 +76,7 @@ export class PlaylistMetadataWidget extends PlaylistWidget implements OnDestroy 
   }
 
   protected onActivate(firstTimeActivating: boolean): void {
-    if (this.isNewData && (this.data.playlistContent || '').trim().length > 0) {
+    if (this.isNewData) {
       this.updateState({ isDirty: true });
     }
 
