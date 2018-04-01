9503bed967cae45e7b1e7b6ba8cfef285b95e4d2
diamond-darrell <diamond-darrell@users.noreply.github.com>
2018-04-01 12:28:54 +0300

    feat(content/entries): add custom metadata component that manages linked entries list KMCNG-293

--- src/applications/content-entries-app/entry/entry-components-list.ts
+++ src/applications/content-entries-app/entry/entry-components-list.ts
@@ -19,8 +19,7 @@ import {EntryComponent} from './entry.component';
 import {EntryPreview} from './entry-preview/entry-preview.component';
 import {EntryDetails} from './entry-details/entry-details.component';
 import {JumpToSection} from './entry-metadata/jump-to-section.component';
-import {LinkedEntries} from './entry-metadata/entry-selector/linked-entries.component';
-import {LinkedEntriesPopup} from './entry-metadata/entry-selector/linked-entries-popup.component';
+import {LinkedEntriesComponent} from './entry-metadata/entry-selector/linked-entries/linked-entries.component';
 import {CategoriesSelector} from './entry-metadata/category-selector/categories-selector.component';
 import {TagsPipe} from './entry-flavours/tags.pipe';
 
@@ -38,6 +37,8 @@ import { EditDistributionProfileMetadataComponent } from './entry-distribution/e
 import { EditDistributionProfileThumbnailsComponent } from './entry-distribution/edit-distribution-profile/edit-distribution-profile-thumbnails/edit-distribution-profile-thumbnails.component';
 import { EditDistributionProfileThumbnailItemComponent } from './entry-distribution/edit-distribution-profile/edit-distribution-profile-thumbnails/thumbnail-item/thumbnail-item.component';import {EntryAdvertisementsComponent} from './entry-advertisements/entry-advertisements.component';
 import {AdvertisementsComponent} from './entry-advertisements/advertisements/advertisements.component';
+import { LinkedEntriesTableComponent } from './entry-metadata/entry-selector/linked-entries-table/linked-entries-table.component';
+import { LinkedEntriesAddEntriesComponent } from './entry-metadata/entry-selector/linked-entries-add-entries/linked-entries-add-entries.component';
 
 
 export const EntryComponentsList = [
@@ -64,8 +65,9 @@ export const EntryComponentsList = [
     FlavorImport,
     FlavorPreview,
     JumpToSection,
-    LinkedEntries,
-    LinkedEntriesPopup,
+    LinkedEntriesComponent,
+    LinkedEntriesTableComponent,
+    LinkedEntriesAddEntriesComponent,
     TagsPipe,
     EntryDistributionComponent,
     DistributedProfileComponent,
