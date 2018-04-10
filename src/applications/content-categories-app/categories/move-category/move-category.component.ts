a566be0b998541cfd479627c3a9daa655c1e5109
Stas <staskgt@gmail.com>
2018-04-10 18:01:43 +0300

    fix "move categories" valiadtion

--- src/applications/content-categories-app/categories/move-category/move-category.component.ts
+++ src/applications/content-categories-app/categories/move-category/move-category.component.ts
@@ -1,15 +1,18 @@
-import {Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
-import {AreaBlockerMessage} from '@kaltura-ng/kaltura-ui';
-import {CategoriesService} from '../categories.service';
-import {PopupWidgetComponent} from '@kaltura-ng/kaltura-ui/popup-widget/popup-widget.component';
-import {AppLocalization} from '@kaltura-ng/kaltura-common';
-import {BrowserService} from 'app-shared/kmc-shell';
-import {KalturaCategory} from 'kaltura-ngx-client/api/types/KalturaCategory';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { AreaBlockerMessage } from '@kaltura-ng/kaltura-ui';
+import { CategoriesService } from '../categories.service';
+import { PopupWidgetComponent } from '@kaltura-ng/kaltura-ui/popup-widget/popup-widget.component';
+import { AppLocalization } from '@kaltura-ng/kaltura-common';
+import { BrowserService } from 'app-shared/kmc-shell';
+import { KalturaCategory } from 'kaltura-ngx-client/api/types/KalturaCategory';
 import {
   CategoriesStatus,
   CategoriesStatusMonitorService
 } from 'app-shared/content-shared/categories-status/categories-status-monitor.service';
 import { SelectedCategory } from 'app-shared/content-shared/categories/category-selector/category-selector.component';
+import { Observable } from 'rxjs/Observable';
+import { CategoriesGraphUpdatedEvent } from 'app-shared/kmc-shared/app-events/categories-graph-updated/categories-graph-updated';
+import { AppEventsService } from 'app-shared/kmc-shared';
 
 @Component({
   selector: 'kMoveCategory',
@@ -27,6 +30,7 @@ export class MoveCategoryComponent implements OnInit, OnDestroy {
   public _categoriesUpdating = false;
 
   constructor(private _categoriesService: CategoriesService,
+              private _appEvents: AppEventsService,
               private _appLocalization: AppLocalization,
               private _browserService: BrowserService,
               private _categoriesStatusMonitorService: CategoriesStatusMonitorService) {
@@ -74,35 +78,66 @@ export class MoveCategoryComponent implements OnInit, OnDestroy {
       return;
     }
 
-    const invalidCategory = this.selectedCategories.find((category) =>
-      (!this._validateCategoryMove(category))
-    );
-    if (!invalidCategory) {
-      this._browserService.confirm(
-        {
-          header: this._appLocalization.get('applications.content.categories.moveCategory'),
-          message: this._appLocalization.get('applications.content.moveCategory.treeUpdateNotification'),
-          accept: () => {
-            this._blockerMessage = null;
-            this._moveCategory();
+    Observable.from(this.selectedCategories)
+      .switchMap(category => this._validateCategoryMove(category))
+      .toArray()
+      .subscribe(
+        validatedCategories => {
+          const allValid = validatedCategories.every(Boolean);
+          if (allValid) {
+            this._browserService.confirm({
+              header: this._appLocalization.get('applications.content.categories.moveCategory'),
+              message: this._appLocalization.get('applications.content.moveCategory.treeUpdateNotification'),
+              accept: () => {
+                this._blockerMessage = null;
+                this._moveCategory();
+              }
+            });
           }
-        }
-      );
-    }
+        },
+        error => {
+          this._browserService.confirm({
+            header: this._appLocalization.get('appp.common.error'),
+            message: error.messaeg || this._appLocalization.get('applications.content.moveCategory.errors.failedToLoadParentCategoryData'),
+            accept: () => {
+              this._blockerMessage = null;
+            }
+          });
+        });
+  }
+
+  private _getCategoryParentData(): Observable<{ id?: number, fullIds: number[] }> {
+    return Observable.create(observer => {
+      const hasSelectedParent = this._selectedParentCategory && this._selectedParentCategory !== 'missing';
+
+      if (!hasSelectedParent) {
+        observer.next({ id: 0, fullIds: [] });
+        observer.complete();
+        return;
+      }
+
+      this._categoriesService.getCategoryById(<number>this._selectedParentCategory)
+        .subscribe(
+          category => {
+            observer.next({ id: category.id, fullIds: category.fullIdPath });
+            observer.complete();
+          },
+          error => {
+            observer.error(error);
+          });
+    });
   }
 
   private _moveCategory() {
-    // TODO sakal - this._selectedParentCategory.fullIdPath
-    const categoryParent = this._selectedParentCategory && this._selectedParentCategory !== 'missing' ?
-      {id: this._selectedParentCategory, fullIds: null} :
-      {id: 0, fullIds: []};
-    this._categoriesService
-      .moveCategory({categories: this.selectedCategories, categoryParent})
+    this._getCategoryParentData()
+      .switchMap(categoryParent => this._categoriesService.moveCategory({ categories: this.selectedCategories, categoryParent }))
       .tag('block-shell')
       .cancelOnDestroy(this)
       .subscribe(() => {
           this.onMovedCategories.emit();
+          this._appEvents.publish(new CategoriesGraphUpdatedEvent());
           this._categoriesStatusMonitorService.updateCategoriesStatus();
+          this._categoriesService.reload();
           if (this.parentPopupWidget) {
             this.parentPopupWidget.close();
           }
@@ -129,43 +164,51 @@ export class MoveCategoryComponent implements OnInit, OnDestroy {
   }
 
   private _validateCategoryMove(categoryToMove: KalturaCategory) {
-    // if category moved to the same parent or to 'no parent' as it was before
-    if (((!this._selectedParentCategory || this._selectedParentCategory === 'missing') && !categoryToMove.parentId) ||
-        (this._selectedParentCategory && this._selectedParentCategory !== 'missing' && categoryToMove.parentId === this._selectedParentCategory)) {
-      this._blockerMessage = new AreaBlockerMessage({
-        message: this._appLocalization.get('applications.content.moveCategory.errors.categoryAlreadyBelongsToParent'),
-        buttons: [
-          {
-            label: this._appLocalization.get('app.common.cancel'),
-            action: () => {
-              this._blockerMessage = null;
-            }
-          }
-        ]
-      });
-      return false;
-      // TODO sakal - this._selectedParentCategory.fullIdPath
-    } else if (this._selectedParentCategory && this._selectedParentCategory !== 'missing' && !this._categoriesService.isParentCategorySelectionValid(
-        {
-          categories: this.selectedCategories,
-          categoryParent: {id: this._selectedParentCategory, fullIds: null}
-        })) {
-      // if trying to move category be a child of itself or one of its children show error message
-      this._blockerMessage = new AreaBlockerMessage({
-        message: this._appLocalization.get('applications.content.moveCategory.errors.invalidParentSelection'),
-        buttons: [
-          {
-            label: this._appLocalization.get('app.common.cancel'),
-            action: () => {
-              this._blockerMessage = null;
-            }
-          }
-        ]
-      });
-      return false;
+    const hasSelectedParent = this._selectedParentCategory && this._selectedParentCategory !== 'missing';
+
+    if (!hasSelectedParent) {
+      return Observable.of(false);
     }
 
-    return true;
+    // if category moved to the same parent or to 'no parent' as it was before
+    const noCurrentParent = !categoryToMove.parentId;
+    const sameParent = hasSelectedParent && categoryToMove.parentId === this._selectedParentCategory;
+    if ((!hasSelectedParent && noCurrentParent) || sameParent) {
+      this._blockerMessage = new AreaBlockerMessage({
+        message: this._appLocalization.get('applications.content.moveCategory.errors.categoryAlreadyBelongsToParent'),
+        buttons: [{
+          label: this._appLocalization.get('app.common.cancel'),
+          action: () => {
+            this._blockerMessage = null;
+          }
+        }]
+      });
+      return Observable.of(false);
+    }
+
+    return this._categoriesService.getCategoryById(<number>this._selectedParentCategory)
+      .map(selectedParent => {
+        return this._categoriesService.isParentCategorySelectionValid(
+          {
+            categories: this.selectedCategories,
+            categoryParent: { id: selectedParent.id, fullIds: selectedParent.fullIdPath }
+          });
+      })
+      .do(isParentCategorySelectionValid => {
+        if (!isParentCategorySelectionValid) {
+          this._blockerMessage = new AreaBlockerMessage({
+            message: this._appLocalization.get('applications.content.moveCategory.errors.invalidParentSelection'),
+            buttons: [
+              {
+                label: this._appLocalization.get('app.common.cancel'),
+                action: () => {
+                  this._blockerMessage = null;
+                }
+              }
+            ]
+          });
+        }
+      });
   }
 
 
