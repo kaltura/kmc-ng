ca5a64c5b6c1e8b50d8b16ca954fec9f35253d85
diamond-darrell <diamond-darrell@users.noreply.github.com>
2018-04-10 10:43:29 +0300

    feat: implement permission dropFolder.CONTENT_INGEST_DROP_FOLDER_DELETE (#533) KMCNG-1586

--- src/applications/content-drop-folders-app/drop-folders-table/drop-folders-table.component.ts
+++ src/applications/content-drop-folders-app/drop-folders-table/drop-folders-table.component.ts
@@ -14,6 +14,7 @@ import {KalturaDropFolderFile} from 'kaltura-ngx-client/api/types/KalturaDropFol
 import {AppLocalization} from '@kaltura-ng/kaltura-common/localization/app-localization.service';
 import {DatePipe} from '@kaltura-ng/kaltura-ui';
 import { globalConfig } from 'config/global';
+import { KMCPermissions } from 'app-shared/kmc-shared/kmc-permissions';
 
 @Component({
   selector: 'kDropFoldersListTable',
@@ -49,6 +50,7 @@ export class DropFoldersTableComponent implements OnInit, AfterViewInit, OnDestr
   public _items: MenuItem[];
   public _emptyMessage = '';
   public _defaultSortOrder = globalConfig.client.views.tables.defaultSortOrder;
+  public _kmcPermissions = KMCPermissions;
 
   constructor(private _appLocalization: AppLocalization,
               private cdRef: ChangeDetectorRef) {
