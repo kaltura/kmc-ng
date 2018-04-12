90a10cd5183a7a7fc947ec77ce1d3280727cee1c
diamond-darrell <diamond-darrell@users.noreply.github.com>
2018-04-12 14:36:09 +0300

    feat: implement permission FEATURE_DISABLE_KMC_KDP_ALERTS (#549) KMCNG-1609

--- src/applications/content-moderation-app/entry-report/entry-report.component.ts
+++ src/applications/content-moderation-app/entry-report/entry-report.component.ts
@@ -72,6 +72,11 @@ export class EntryReportComponent implements OnInit, OnDestroy {
       entryid: this.entryId,
       flashvars: {'closedCaptions': { 'plugin': true }, 'ks': this.appAuthentication.appUser.ks}
     };
+
+    const shouldDisableAlerts = this._permissionsService.hasPermission(KMCPermissions.FEATURE_DISABLE_KMC_KDP_ALERTS);
+    if (shouldDisableAlerts) {
+      this._playerConfig.flashvars['disableAlerts'] = true;
+    }
   }
 
   ngOnDestroy() {
