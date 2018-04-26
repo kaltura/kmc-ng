38ba2f404cc8708a3a9a9a9a1ec0b9c9c2b1e426
Stas <staskgt@gmail.com>
2018-04-26 15:09:10 +0300

    redirect to https if configs say so

--- src/kmc-app/main.ts
+++ src/kmc-app/main.ts
@@ -5,35 +5,36 @@ import { environment } from 'environments/environment';
 import { initializeConfiguration } from '../configuration/server-config-utils';
 import { globalConfig } from 'config/global';
 
-initializeConfiguration()
-    .subscribe(
-        () =>
-        {
-            platformBrowserDynamic().bootstrapModule(AppModule);
-        },
-        (error) =>
-        {
-            try {
-                const appContainer = document.getElementById('appContainer');
-                if (appContainer) {
-                    appContainer.remove();
-                }
-
-                const errorElement = document.getElementById('appError');
-                if (errorElement) {
-                    errorElement.style.display = 'block';
-                }
-            }catch(innerError) {
-            }
-
-            console.error(error);
-        }
-    );
-
-if (environment.production) {
-    enableProdMode();
-    console.log(`Running KMCng version '${globalConfig.client.appVersion}' (Production mode)`);
+if (environment.client.useSecuredProtocol && location.protocol !== 'https:') {
+    location.protocol = 'https:';
 } else {
-    console.log(`Running KMCng version '${globalConfig.client.appVersion}' (Development mode)`);
-}
+    initializeConfiguration()
+        .subscribe(
+            () => {
+                platformBrowserDynamic().bootstrapModule(AppModule);
+            },
+            (error) => {
+                try {
+                    const appContainer = document.getElementById('appContainer');
+                    if (appContainer) {
+                        appContainer.remove();
+                    }
 
+                    const errorElement = document.getElementById('appError');
+                    if (errorElement) {
+                        errorElement.style.display = 'block';
+                    }
+                } catch (innerError) {
+                }
+
+                console.error(error);
+            }
+        );
+
+    if (environment.production) {
+        enableProdMode();
+        console.log(`Running KMCng version '${globalConfig.client.appVersion}' (Production mode)`);
+    } else {
+        console.log(`Running KMCng version '${globalConfig.client.appVersion}' (Development mode)`);
+    }
+}
