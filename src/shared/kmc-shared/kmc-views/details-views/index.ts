c65d4cc5ecda45db9bca52f508ae9989d23c4297
Stas <staskgt@gmail.com>
2018-04-27 10:01:37 +0300

    implement component views for external apps

--- src/shared/kmc-shared/kmc-views/details-views/index.ts
+++ src/shared/kmc-shared/kmc-views/details-views/index.ts
@@ -2,3 +2,4 @@ export * from './content-category-view.service';
 export * from './content-playlist-view.service';
 export * from './settings-transcoding-profile-view.service';
 export * from './content-entry-view.service';
+export * from './content-new-category-view.service';
