30900759685f5432f39ad98ba2f7e5e3ad8b1f36
Eran Sakal <eran@sakalim.com>
2018-05-02 10:45:48 +0300

    feat: support CDN configuration provided at runtime

--- src/kmc-app/main.ts
+++ src/kmc-app/main.ts
@@ -6,6 +6,18 @@ import { initializeConfiguration } from '../configuration/server-config-utils';
 import { globalConfig } from 'config/global';
 import { externalAppsConfigurationAdapter } from 'config/server';
 
+// if CDN is used, change the base uri being used by webpack to fetch static assets
+// this feature reuires manipulation on the index.html as well so it should be used
+// in production only.
+declare var __webpack_require__: any;
+declare var kmcConfig;
+
+(function() {
+    if (kmcConfig && kmcConfig.kalturaServer && kmcConfig.kalturaServer.deployUrl) {
+
+        __webpack_require__.p = kmcConfig.kalturaServer.deployUrl;
+    }
+}());
 
 if (environment.production) {
     enableProdMode();
@@ -13,6 +25,7 @@ if (environment.production) {
 } else {
     console.log(`Running KMCng version '${globalConfig.client.appVersion}' (Development mode)`);
 }
+
 
 initializeConfiguration(externalAppsConfigurationAdapter)
     .subscribe(
