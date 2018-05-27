b2f7007a7e8079459fbfb5e64a142086bf0aa33b
Stas Kohut <diamond-darrell@users.noreply.github.com>
2018-05-27 14:44:40 +0300

    chore(content/categories): add logs (#624)

--- src/applications/content-categories-app/categories/categories-list/categories-list-tags.component.ts
+++ src/applications/content-categories-app/categories/categories-list/categories-list-tags.component.ts
@@ -6,6 +6,7 @@ import {RefineGroup, RefineGroupList} from '../categories-refine-filters.service
 import {CategoriesSearchService} from 'app-shared/content-shared/categories/categories-search.service';
 import {ISubscription} from 'rxjs/Subscription';
 import {DatePipe} from '@kaltura-ng/kaltura-ui';
+import { KalturaLogger } from '@kaltura-ng/kaltura-logger/kaltura-logger.service';
 
 export interface TagItem {
   type: string,
@@ -20,7 +21,8 @@ const refineListsTypes: Array<keyof CategoriesFilters> = ['privacyTypes', 'categ
 @Component({
   selector: 'kCategoriesListTags',
   templateUrl: './categories-list-tags.component.html',
-  styleUrls: ['./categories-list-tags.component.scss']
+  styleUrls: ['./categories-list-tags.component.scss'],
+    providers: [KalturaLogger.createLogger('CategoriesListTagsComponent')]
 
 })
 export class CategoriesListTagsComponent implements OnInit, OnDestroy {
@@ -45,10 +47,14 @@ export class CategoriesListTagsComponent implements OnInit, OnDestroy {
     public _showTags = false;
 
 
-  constructor(private _categoriesService: CategoriesService, private _appLocalization: AppLocalization, private _categoriesSearch: CategoriesSearchService) {
+  constructor(private _categoriesService: CategoriesService,
+              private _appLocalization: AppLocalization,
+              private _categoriesSearch: CategoriesSearchService,
+              private _logger: KalturaLogger) {
   }
 
   removeTag(tag: any) {
+      this._logger.info(`handle remove tag action by user`, { type: tag.type, value: tag.value });
 
       if (tag.dataFetchSubscription)
       {
@@ -97,6 +103,7 @@ export class CategoriesListTagsComponent implements OnInit, OnDestroy {
   }
 
   removeAllTags() {
+      this._logger.info(`handle remove all tags action by user`);
     this._categoriesService.resetFilters();
   }
 
