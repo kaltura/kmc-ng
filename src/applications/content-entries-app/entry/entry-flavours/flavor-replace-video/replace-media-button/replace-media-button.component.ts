b5603a01b196194dc161fc965c40685c20e53155
Stas <staskgt@gmail.com>
2018-05-31 17:28:53 +0300

    update design

--- src/applications/content-entries-app/entry/entry-flavours/flavor-replace-video/replace-media-button/replace-media-button.component.ts
+++ src/applications/content-entries-app/entry/entry-flavours/flavor-replace-video/replace-media-button/replace-media-button.component.ts
@@ -1,7 +1,5 @@
 import { Component, Input, ViewChild } from '@angular/core';
 import { KalturaMediaEntry } from 'kaltura-ngx-client/api/types/KalturaMediaEntry';
-import { KalturaMediaType } from 'kaltura-ngx-client/api/types/KalturaMediaType';
-import { KalturaEntryStatus } from 'kaltura-ngx-client/api/types/KalturaEntryStatus';
 import { KMCPermissions, KMCPermissionsService } from 'app-shared/kmc-shared/kmc-permissions';
 import { PopupWidgetComponent } from '@kaltura-ng/kaltura-ui/popup-widget/popup-widget.component';
 import { Flavor } from '../../flavor';
@@ -21,43 +19,15 @@ export class ReplaceMediaButtonComponent {
     @Input() flavors: Flavor[] = [];
 
     @ViewChild('uploadMenu') _uploadMenu: PopupWidgetComponent;
-    @ViewChild('replaceVideoPopup') _replaceVideoPopup: PopupWidgetComponent;
 
-    public _uploadFileLabel: string;
-    public _importFileLabel: string;
     public _replaceType: UploadMenuType;
-
     public _uploadEnabled = false;
     public _importEnabled = false;
     public _linkEnabled = false;
 
-    public get _actionBtnTitle(): string {
-        if (!this.entry) {
-            return '';
-        }
-
-        if (this.entry.mediaType === KalturaMediaType.video) {
-            return this.entry.status === KalturaEntryStatus.noContent
-                ? this._appLocalization.get('applications.content.entryDetails.flavours.replaceVideo.addVideo')
-                : this._appLocalization.get('applications.content.entryDetails.flavours.replaceVideo.replaceVideo');
-        } else if (this.entry.mediaType === KalturaMediaType.audio) {
-            return this.entry.status === KalturaEntryStatus.noContent
-                ? this._appLocalization.get('applications.content.entryDetails.flavours.replaceVideo.addAudio')
-                : this._appLocalization.get('applications.content.entryDetails.flavours.replaceVideo.replaceAudio');
-        }
-    }
-
     constructor(private _appLocalization: AppLocalization,
                 private _logger: KalturaLogger,
                 private _permissionsService: KMCPermissionsService) {
-        if (this._permissionsService.hasPermission(KMCPermissions.FEATURE_MULTI_FLAVOR_INGESTION)) {
-            this._uploadFileLabel = this._appLocalization.get('applications.content.entryDetails.flavours.replaceVideo.uploadFiles');
-            this._importFileLabel = this._appLocalization.get('applications.content.entryDetails.flavours.replaceVideo.importFiles');
-        } else {
-            this._uploadFileLabel = this._appLocalization.get('applications.content.entryDetails.flavours.replaceVideo.uploadFile');
-            this._importFileLabel = this._appLocalization.get('applications.content.entryDetails.flavours.replaceVideo.importFile');
-        }
-
         this._uploadEnabled = this._permissionsService.hasPermission(KMCPermissions.CONTENT_INGEST_UPLOAD)
             && this._permissionsService.hasPermission(KMCPermissions.CONTENT_INGEST_BASE);
         this._importEnabled = this._permissionsService.hasPermission(KMCPermissions.CONTENT_INGEST_BULK_UPLOAD)
@@ -70,7 +40,6 @@ export class ReplaceMediaButtonComponent {
     public _openReplacementMenu(type: UploadMenuType): void {
         this._logger.info(`handle open replacement menu action by user`, { type });
         this._replaceType = type;
-        this._replaceVideoPopup.close();
         this._uploadMenu.open();
     }
 }
