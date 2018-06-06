edf6e9c8c38498786d249b55b6403afc5167da81
Stas <staskgt@gmail.com>
2018-06-06 16:29:36 +0300

    fix schema

--- src/shared/kmc-shared/contextual-help/contextual-help-data-schema.ts
+++ src/shared/kmc-shared/contextual-help/contextual-help-data-schema.ts
@@ -1,18 +1,23 @@
 export const ContextualHelpDataSchema = {
     type: 'array',
-    items: [{
-        viewKey: { type: 'string' },
-        links: {
-            type: 'array',
-            items: [{
-                type: 'object',
-                properties: {
-                    label: { type: 'string' },
-                    value: { type: 'string' }
-                },
-                required: ['label', 'value'],
-                additionalProperties: false
-            }]
-        }
-    }]
+    items: {
+        type: 'object',
+        properties: {
+            viewKey: { type: 'string' },
+            links: {
+                type: 'array',
+                items: {
+                    type: 'object',
+                    properties: {
+                        label: { type: 'string' },
+                        value: { type: 'string' }
+                    },
+                    required: ['label', 'value'],
+                    additionalProperties: false
+                }
+            }
+        },
+        required: ['viewKey', 'links'],
+        additionalProperties: false
+    }
 };
