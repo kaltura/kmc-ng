a79186bba8539ce45ab6ed4be6a16878722eedc7
Stas <staskgt@gmail.com>
2018-06-25 17:16:33 +0300

    show retry dialog on failed flavors load

--- src/applications/content-entries-app/entries/bulk-actions/services/bulk-download.service.ts
+++ src/applications/content-entries-app/entries/bulk-actions/services/bulk-download.service.ts
@@ -1,53 +1,36 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { KalturaClient } from 'kaltura-ngx-client';
-
-import { KalturaMediaEntry } from 'kaltura-ngx-client';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { KalturaClient, KalturaMediaEntry } from 'kaltura-ngx-client';
 import { BulkActionBaseService } from './bulk-action-base.service';
 import { XInternalXAddBulkDownloadAction } from './XInternalXAddBulkDownloadAction';
 import { AppLocalization } from '@kaltura-ng/mc-shared';
 
 export class BulkDownloadError extends Error {
-  type = 'bulkDownload';
+    type = 'bulkDownload';
 
-  constructor(message: string) {
-    super(message);
-  }
+    constructor(message: string) {
+        super(message);
+    }
 }
 
 @Injectable()
 export class BulkDownloadService extends BulkActionBaseService<number> {
+    constructor(_kalturaServerClient: KalturaClient, private _appLocalization: AppLocalization) {
+        super(_kalturaServerClient);
+    }
 
-  constructor(_kalturaServerClient: KalturaClient, private _appLocalization: AppLocalization) {
-    super(_kalturaServerClient);
-  }
-
-  public execute(selectedEntries: KalturaMediaEntry[], flavorId: number) : Observable<{}>{
-    return Observable.create(observer =>{
-
-      let requests: XInternalXAddBulkDownloadAction[] = [];
-      let entryIds = "";
-
-      selectedEntries.forEach(entry => {
-        entryIds += entry.id +",";
-      });
-      if (entryIds.lastIndexOf(",") === entryIds.length - 1) {
-        entryIds = entryIds.substr(0, entryIds.length - 1); // remove last comma
-      }
-
-      this._kalturaServerClient.request(new XInternalXAddBulkDownloadAction({
-        entryIds: entryIds,
-        flavorParamsId: flavorId.toString()
-      })).subscribe(
-          result => {
-            observer.next({'email': result})
-            observer.complete();
-          },
-          error => {
-            observer.error(new BulkDownloadError(this._appLocalization.get('applications.content.bulkActions.downloadFailed')));
-          }
-      );
-    });
-  }
-
+    public execute(selectedEntries: KalturaMediaEntry[], flavorId: number): Observable<{ email: string }> {
+        return this._kalturaServerClient
+            .request(new XInternalXAddBulkDownloadAction({
+                entryIds: selectedEntries.join(','),
+                flavorParamsId: flavorId.toString()
+            }))
+            .pipe(
+                map(email => ({ email })),
+                catchError(() =>
+                    throwError(new BulkDownloadError(this._appLocalization.get('applications.content.bulkActions.downloadFailed')))
+                )
+            );
+    }
 }
