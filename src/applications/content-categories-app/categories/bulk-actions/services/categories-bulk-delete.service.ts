bca789d1bfe7d08a4af285ec219f79ae0ec9363c
Stas <staskgt@gmail.com>
2018-07-09 15:12:31 +0300

    show relevant error message for bulk actions

--- src/applications/content-categories-app/categories/bulk-actions/services/categories-bulk-delete.service.ts
+++ src/applications/content-categories-app/categories/bulk-actions/services/categories-bulk-delete.service.ts
@@ -1,41 +1,23 @@
 import {Injectable} from '@angular/core';
 import { Observable } from 'rxjs';
-import {KalturaClient} from 'kaltura-ngx-client';
-import {CategoriesBulkActionBaseService} from "./categories-bulk-action-base.service";
-import {KalturaCategory} from 'kaltura-ngx-client';
-import {CategoryDeleteAction} from 'kaltura-ngx-client';
+import { map } from 'rxjs/operators';
+import { CategoriesBulkActionBaseService } from './categories-bulk-action-base.service';
+import { KalturaClient, KalturaCategory, CategoryDeleteAction} from 'kaltura-ngx-client';
 import { CategoriesGraphUpdatedEvent } from 'app-shared/kmc-shared/app-events/categories-graph-updated/categories-graph-updated';
 import { AppEventsService } from 'app-shared/kmc-shared';
 
 @Injectable()
-export class CategoriesBulkDeleteService extends CategoriesBulkActionBaseService<{}> {
+export class CategoriesBulkDeleteService extends CategoriesBulkActionBaseService<void> {
 
   constructor(_kalturaServerClient: KalturaClient, private _appEvents: AppEventsService) {
     super(_kalturaServerClient);
   }
 
-  public execute(selectedCategories: KalturaCategory[]) : Observable<{}>{
-    return Observable.create(observer =>{
-
-      const requests: CategoryDeleteAction[] = [];
-
-      selectedCategories.forEach(category => {
-        requests.push(new CategoryDeleteAction({
-          id: category.id
-        }));
-      });
-
-      this.transmit(requests, true).subscribe(
-        result => {
-
-            this._appEvents.publish(new CategoriesGraphUpdatedEvent());
-          observer.next({});
-          observer.complete();
-        },
-        error => {
-          observer.error(error);
-        }
-      );
-    });
+  public execute(selectedCategories: KalturaCategory[]): Observable<void> {
+      const requests = selectedCategories.map(category => new CategoryDeleteAction({ id: category.id }));
+      return this.transmit(requests, true)
+          .pipe(map(() => {
+              this._appEvents.publish(new CategoriesGraphUpdatedEvent());
+          }));
   }
 }
