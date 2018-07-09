bb5059c2b0c6c0408d2e01d2bfb9ad4749f0af45
Stas <staskgt@gmail.com>
2018-07-09 14:42:38 +0300

    remove file extension from the entry's name

--- src/applications/kmc-upload-app/upload-settings/upload-settings.component.ts
+++ src/applications/kmc-upload-app/upload-settings/upload-settings.component.ts
@@ -90,7 +90,8 @@ export class UploadSettingsComponent implements OnInit, AfterViewInit {
     const newItems = Array.from(files).map(file => {
       const ext = this._getFileExtension(file.name);
       const mediaType = this._getMediaTypeFromExtension(ext);
-      const { name, size } = file;
+      const name = file.name.replace(/(?:\.([^.]+))?$/, '');
+      const { size } = file;
       return ({ file, mediaType, name, size, isEditing });
     });
 
