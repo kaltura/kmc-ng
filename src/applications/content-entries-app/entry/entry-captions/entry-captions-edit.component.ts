ee8c492bbf09bcdfc12eb4b537964587e92bbd1c
Amir Chervinsky <amir.chervinsky@kaltura.com>
2018-07-22 15:20:37 +0300

    fix(content/entry): fix entry caption language selection upon editing (#781)

--- src/applications/content-entries-app/entry/entry-captions/entry-captions-edit.component.ts
+++ src/applications/content-entries-app/entry/entry-captions/entry-captions-edit.component.ts
@@ -80,7 +80,7 @@ export class EntryCaptionsEdit implements  OnInit, AfterContentInit, OnDestroy{
 						this._validationErrorMsg = "";
 						this.fileToUpload = null;
 						this.captionsEditForm.get("label").setValue(this.currentCaption.label);
-						this.captionsEditForm.get("language").setValue(this.currentCaption.language);
+						this.captionsEditForm.get("language").setValue(this._languageOptions.getValueByLabel(this.currentCaption.language));
 						this.captionsEditForm.get("format").setValue(this.currentCaption.format);
 					}
 					if (event.state === PopupWidgetStates.BeforeClose) {
