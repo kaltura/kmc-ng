a529a08ee70d4ae9854aad6d093c2ae9642b97a0
amirch1 <amir.chervinsky@kaltura.com>
2018-10-25 09:11:11 +0300

    chore: update version of deployed assets

--- src/configuration/global-config.ts
+++ src/configuration/global-config.ts
@@ -26,7 +26,7 @@ export interface GlobalConfig {
 export const globalConfig: GlobalConfig = {
     client: {
         production: environment.production,
-        appVersion: '5.4.2',
+        appVersion: '5.5.0',
         useSecuredProtocol: environment.client.useSecuredProtocol,
         countriesList: countryCodes,
         views: {
