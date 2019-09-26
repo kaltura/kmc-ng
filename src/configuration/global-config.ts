665b8d4d4827d50bbffde04374db08e70519d2d5
Stas <staskgt@gmail.com>
2019-09-26 14:35:39 +0300

    chore: bump version

--- src/configuration/global-config.ts
+++ src/configuration/global-config.ts
@@ -26,7 +26,7 @@ export interface GlobalConfig {
 export const globalConfig: GlobalConfig = {
     client: {
         production: environment.production,
-        appVersion: '6.0.3',
+        appVersion: '6.0.4',
         useSecuredProtocol: environment.client.useSecuredProtocol,
         countriesList: countryCodes,
         views: {
