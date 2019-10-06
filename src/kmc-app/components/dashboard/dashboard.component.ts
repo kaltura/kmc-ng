2dc015854dc2cff987fa07b460ddfe6c4fd1dff4
amir.chervinsky <amir.chervinsky@kaltura.com>
2019-10-06 09:32:56 +0300

    kaltura connect promo

--- src/kmc-app/components/dashboard/dashboard.component.ts
+++ src/kmc-app/components/dashboard/dashboard.component.ts
@@ -32,10 +32,10 @@ export class DashboardComponent implements OnInit, AfterViewInit, OnDestroy {
 
   private _showWhatsNew(): void {
       const isRegisteredUser = this.appAuthentication.appUser.partnerInfo.partnerPackage !== PartnerPackageTypes.PartnerPackageFree;
-      const whatsNewShown = this._browserService.getFromLocalStorage('whatsNewShown') || false;
+      const whatsNewShown = this._browserService.getFromLocalStorage('connectShown') || false;
       if (isRegisteredUser && !whatsNewShown){
           setTimeout(()=>{
-              this._browserService.setInLocalStorage('whatsNewShown',true);
+              this._browserService.setInLocalStorage('connectShown',true);
               this._whatsNewWin.open();
           },200);
       }
@@ -45,11 +45,15 @@ export class DashboardComponent implements OnInit, AfterViewInit, OnDestroy {
         this._whatsNewWin.close();
     }
 
+    register():void{
+        this._browserService.openLink('https://connect.kaltura.com');
+    }
+
   ngAfterViewInit()
   {
     $(window).bind('resize', this.onResize); // We bind the event to a function reference that proxy 'actual' this inside
     this._resizeContent();
-    // this._showWhatsNew();
+    this._showWhatsNew();
   }
 
   ngOnInit() {
