41a3993198b3fdbe56aa89be50c70564fe9ee296
amir.chervinsky <amir.chervinsky@kaltura.com>
2019-10-14 07:59:13 +0300

    chore: add Kaltura connect promo screen to the whats new window

--- src/kmc-app/components/dashboard/dashboard.component.ts
+++ src/kmc-app/components/dashboard/dashboard.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, AfterViewInit, ViewChild, OnDestroy } from '@angular/core';
 import { AppAuthentication, AppShellService, BrowserService, PartnerPackageTypes } from "app-shared/kmc-shell";
-
+import { buildDeployUrl } from 'config/server';
 import * as $ from 'jquery';
 import { PopupWidgetComponent } from '@kaltura-ng/kaltura-ui';
 
@@ -12,13 +12,13 @@ import { PopupWidgetComponent } from '@kaltura-ng/kaltura-ui';
 export class DashboardComponent implements OnInit, AfterViewInit, OnDestroy {
   @ViewChild('appMenu', { static: true }) private _appMenuRef : any;
   @ViewChild('whatsNew', { static: true }) private _whatsNewWin : PopupWidgetComponent;
+
+  public _bannerUri = buildDeployUrl('./assets/kaltura_connect_banner2x.png');
   private onResize : () => void;
 
 
   constructor(private appShellService : AppShellService, private appAuthentication: AppAuthentication, private _browserService: BrowserService) {
       this.onResize = this._resizeContent.bind(this);
-
-
   }
 
   private _resizeContent() : void
@@ -32,10 +32,10 @@ export class DashboardComponent implements OnInit, AfterViewInit, OnDestroy {
 
   private _showWhatsNew(): void {
       const isRegisteredUser = this.appAuthentication.appUser.partnerInfo.partnerPackage !== PartnerPackageTypes.PartnerPackageFree;
-      const whatsNewShown = this._browserService.getFromLocalStorage('whatsNewShown') || false;
+      const whatsNewShown = this._browserService.getFromLocalStorage('connectPromoShown') || false;
       if (isRegisteredUser && !whatsNewShown){
           setTimeout(()=>{
-              this._browserService.setInLocalStorage('whatsNewShown',true);
+              this._browserService.setInLocalStorage('connectPromoShown',true);
               this._whatsNewWin.open();
           },200);
       }
@@ -45,11 +45,15 @@ export class DashboardComponent implements OnInit, AfterViewInit, OnDestroy {
         this._whatsNewWin.close();
     }
 
+    register():void{
+        this._browserService.openLink('https://connect.kaltura.com');
+    }
+
   ngAfterViewInit()
   {
     $(window).bind('resize', this.onResize); // We bind the event to a function reference that proxy 'actual' this inside
     this._resizeContent();
-    // this._showWhatsNew();
+    this._showWhatsNew();
   }
 
   ngOnInit() {
