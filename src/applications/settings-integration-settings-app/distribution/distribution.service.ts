7782938131c6329aaac308cb5390c212292db86f
amir.chervinsky <amir.chervinsky@kaltura.com>
2020-05-11 11:43:29 +0300

    add Youtube distribution profiles management

---
+++ src/applications/settings-integration-settings-app/distribution/distribution.service.ts
@@ -0,0 +1,30 @@
+import { Injectable, OnDestroy } from '@angular/core';
+import { DistributionProfileDeleteAction, DistributionProfileListAction, KalturaClient, KalturaDistributionProfileFilter,
+    KalturaDistributionProfileListResponse, KalturaDistributionProfileStatus, KalturaFilterPager } from 'kaltura-ngx-client';
+import { Observable } from 'rxjs';
+import { cancelOnDestroy } from '@kaltura-ng/kaltura-common';
+
+@Injectable()
+export class DistributionService implements OnDestroy{
+
+  constructor(private _kalturaServerClient: KalturaClient) {
+  }
+
+  public loadDistributionProfiles(): Observable<KalturaDistributionProfileListResponse> {
+    const pager = new KalturaFilterPager({pageSize: 500, pageIndex: 0});
+    const filter = new KalturaDistributionProfileFilter({statusEqual: KalturaDistributionProfileStatus.enabled})
+    const request = new DistributionProfileListAction({pager, filter});
+    return this._kalturaServerClient.request(request).pipe(cancelOnDestroy(this));
+  }
+
+  public deleteProfile(id: number): Observable<void> {
+      if (!id) {
+          return Observable.throwError(new Error('Error occurred while trying to delete profile'));
+      }
+      return this._kalturaServerClient.request(new DistributionProfileDeleteAction({id})).pipe(cancelOnDestroy(this));
+  }
+
+  ngOnDestroy() {
+  }
+
+}
