b1fe1ca196bd9242c6a2237ebe36e2fbca0b783c
amirch1 <amir.chervinsky@kaltura.com>
2021-01-18 13:48:29 +0200

    feat(settings/reach): add captions label suffix field

--- src/applications/settings-reach-app/reach-profile/reach-profile-service/reach-profile-service-widget.service.ts
+++ src/applications/settings-reach-app/reach-profile/reach-profile-service/reach-profile-service-widget.service.ts
@@ -12,9 +12,9 @@ import {KalturaMultiRequest, KalturaReachProfile, KalturaVendorCatalogItemOutput
 
 @Injectable()
 export class ReachProfileServiceWidget extends ReachProfileWidget implements OnDestroy {
-    
+
     public formatOptions: { label: string, value: number }[] = [];
-    
+
     public serviceForm: FormGroup;
     public formatField: AbstractControl;
     public maxField: AbstractControl;
@@ -24,17 +24,21 @@ export class ReachProfileServiceWidget extends ReachProfileWidget implements OnD
     public speakerField: AbstractControl;
     public audioField: AbstractControl;
     public removalField: AbstractControl;
-    
+    public useMachineCaptionsLabelField: AbstractControl;
+    public machineCaptionsLabelField: AbstractControl;
+    public useHumanCaptionsLabelField: AbstractControl;
+    public humanCaptionsLabelField: AbstractControl;
+
     constructor(logger: KalturaLogger,
                 private _permissionsService: KMCPermissionsService,
                 private _appLocalization: AppLocalization,
                 private _formBuilder: FormBuilder) {
-        
+
         super(SettingsReachProfileViewSections.Service, logger);
         this._buildForm();
     }
-    
-    
+
+
     private _buildForm(): void {
         this.serviceForm = this._formBuilder.group({
             format: '',
@@ -44,9 +48,13 @@ export class ReachProfileServiceWidget extends ReachProfileWidget implements OnD
             metadata: '',
             speaker: '',
             audio: '',
-            removal: ''
+            removal: '',
+            useMachineCaptionsLabel: '',
+            machineCaptionsLabel: '',
+            useHumanCaptionsLabel: '',
+            humanCaptionsLabel: ''
         });
-        
+
         this.formatField = this.serviceForm.controls['format'];
         this.maxField = this.serviceForm.controls['max'];
         this.machineField = this.serviceForm.controls['machine'];
@@ -55,8 +63,12 @@ export class ReachProfileServiceWidget extends ReachProfileWidget implements OnD
         this.speakerField = this.serviceForm.controls['speaker'];
         this.audioField = this.serviceForm.controls['audio'];
         this.removalField = this.serviceForm.controls['removal'];
+        this.useMachineCaptionsLabelField = this.serviceForm.controls['useMachineCaptionsLabel'];
+        this.machineCaptionsLabelField = this.serviceForm.controls['machineCaptionsLabel'];
+        this.useHumanCaptionsLabelField = this.serviceForm.controls['useHumanCaptionsLabel'];
+        this.humanCaptionsLabelField = this.serviceForm.controls['humanCaptionsLabel'];
     }
-    
+
     private _monitorFormChanges(): void {
         Observable.merge(this.serviceForm.valueChanges, this.serviceForm.statusChanges)
             .pipe(cancelOnDestroy(this))
@@ -69,17 +81,17 @@ export class ReachProfileServiceWidget extends ReachProfileWidget implements OnD
                 }
             );
     }
-    
+
     protected onValidate(wasActivated: boolean): Observable<{ isValid: boolean }> {
         const formData = wasActivated ? this.serviceForm.value : this.data;
         const max = (formData.max.toString() || '').trim();
         const hasValue = max !== '';
-    
+
         return Observable.of({
             isValid: hasValue
         });
     }
-    
+
     protected onDataSaving(newData: KalturaReachProfile, request: KalturaMultiRequest): void {
         const formData = this.wasActivated ? this.serviceForm.value : this.data;
         newData.defaultOutputFormat = formData.format;
@@ -90,8 +102,18 @@ export class ReachProfileServiceWidget extends ReachProfileWidget implements OnD
         newData.enableSpeakerChangeIndication = formData.speaker;
         newData.enableAudioTags = formData.audio;
         newData.enableProfanityRemoval = formData.removal;
+        if (formData.useMachineCaptionsLabel) {
+            newData.labelAdditionForMachineServiceType = formData.machineCaptionsLabel;
+        } else {
+            newData.labelAdditionForMachineServiceType = '';
+        }
+        if (formData.useHumanCaptionsLabel) {
+            newData.labelAdditionForHumanServiceType = formData.humanCaptionsLabel;
+        } else {
+            newData.labelAdditionForHumanServiceType = '';
+        }
     }
-    
+
     /**
      * Do some cleanups if needed once the section is removed
      */
@@ -99,18 +121,18 @@ export class ReachProfileServiceWidget extends ReachProfileWidget implements OnD
         this.formatOptions = [];
         this.serviceForm.reset();
     }
-    
+
     protected onActivate(firstTimeActivating: boolean): Observable<{ failed: boolean }> | void {
-        
+
         if (firstTimeActivating && (this.isNewData || this._permissionsService.hasPermission(KMCPermissions.REACH_PLUGIN_PERMISSION))) {
             this._monitorFormChanges();
         }
-        
+
         this.formatOptions = [
             {label: this._appLocalization.get('applications.settings.reach.service.formatOptions.dfxp'), value: KalturaVendorCatalogItemOutputFormat.dfxp},
             {label: this._appLocalization.get('applications.settings.reach.service.formatOptions.srt'), value: KalturaVendorCatalogItemOutputFormat.srt}
         ];
-        
+
         this.serviceForm.reset({
             format: this.data.defaultOutputFormat,
             max: this.data.maxCharactersPerCaptionLine,
@@ -119,9 +141,13 @@ export class ReachProfileServiceWidget extends ReachProfileWidget implements OnD
             metadata: this.data.enableMetadataExtraction,
             speaker: this.data.enableSpeakerChangeIndication,
             audio: this.data.enableAudioTags,
-            removal: this.data.enableProfanityRemoval
+            removal: this.data.enableProfanityRemoval,
+            useMachineCaptionsLabel: this.data.labelAdditionForMachineServiceType && this.data.labelAdditionForMachineServiceType.length,
+            machineCaptionsLabel: this.data.labelAdditionForMachineServiceType || this._appLocalization.get('applications.settings.reach.settings.defaultMachineCaptionsLabel'),
+            useHumanCaptionsLabel: this.data.labelAdditionForHumanServiceType && this.data.labelAdditionForHumanServiceType.length,
+            humanCaptionsLabel: this.data.labelAdditionForHumanServiceType
         });
     }
-    
+
     ngOnDestroy() {}
 }
