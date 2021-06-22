e47cd9db1a3c7ff09588749774d6210617c26f40
Viktor Kyryltsev <85935219+viktor-kyryltsev-kaltura@users.noreply.github.com>
2021-06-22 10:03:10 +0300

    fix(entries/edit/flavors/): Match from Drop Folder error due to old RxJS syntax (use new RxJS syntax) (#947)

--- src/applications/content-entries-app/entry/entry-flavours/flavor-replace-video/match-drop-folder/match-drop-folder.component.ts
+++ src/applications/content-entries-app/entry/entry-flavours/flavor-replace-video/match-drop-folder/match-drop-folder.component.ts
@@ -245,8 +245,10 @@ export class MatchDropFolderComponent implements OnInit, OnDestroy {
         const dropFolderFilesListAction = new DropFolderFileListAction({ filter });
         return this._kalturaClient
             .request(dropFolderFilesListAction)
-            .map(response =>
-                this.flavor ? this._mapDropFolderFilesForFlavor(response) : this._mapDropFolderFiles(response)
+            .pipe(
+                map(response =>
+                    this.flavor ? this._mapDropFolderFilesForFlavor(response) : this._mapDropFolderFiles(response)
+                )
             );
     }
 
@@ -318,7 +320,9 @@ export class MatchDropFolderComponent implements OnInit, OnDestroy {
 
         return this._kalturaClient
             .request(conversionProfileAssetParamsListAction)
-            .map(res => res.objects);
+            .pipe(
+                map(res => res.objects)
+            );
     }
 
     private _loadConversionProfilesWithAssets(): Observable<ConversionProfileWithAssets[]> {
