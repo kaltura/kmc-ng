26609312d8cb245d38f3e201a9e4987150a2714a
amirch1 <amir.chervinsky@kaltura.com>
2021-12-23 10:51:06 +0200

    fix: display self serve on boarding video the first time the app loads KMCNG-2391

--- src/kmc-app/components/dashboard/dashboard.component.ts
+++ src/kmc-app/components/dashboard/dashboard.component.ts
@@ -13,18 +13,14 @@ export class DashboardComponent implements OnInit, AfterViewInit, OnDestroy {
   @ViewChild('appMenu', { static: true }) private _appMenuRef : any;
   @ViewChild('whatsNew', { static: true }) private _whatsNewWin : PopupWidgetComponent;
 
-  public _ks = '';
   public _uiconf = serverConfig.kalturaServer.previewUIConfV7;
-  public _entryId = '1_alj2q99a'; // TODO [selfServe] - get entry from product
-  public _pid = '1645161'; // TODO [selfServe] - get pid from product
+  public _entryId = '1_rickx95w';
+  public _pid = '811441';
   public _cdnUrl = buildCDNUrl("");
-  public _showUnmuteBtn = false;
   private onResize : () => void;
-
 
   constructor(private appShellService : AppShellService, private appAuthentication: AppAuthentication, private _browserService: BrowserService) {
       this.onResize = this._resizeContent.bind(this);
-      this._ks = this.appAuthentication.appUser.ks;
   }
 
   private _resizeContent() : void
@@ -37,11 +33,11 @@ export class DashboardComponent implements OnInit, AfterViewInit, OnDestroy {
   }
 
   private _showWhatsNew(): void {
-      const isVpaasUser = true; // TODO [selfServe] - set logic to show to vpaas only
+      const isSelfserve = this.appAuthentication.appUser.partnerInfo.isSelfServe;
       const whatsNewShown = this._browserService.getFromLocalStorage('getStartedShown') || false;
-      if (isVpaasUser && !whatsNewShown){
+      if (isSelfserve && !whatsNewShown){
           setTimeout(()=>{
-              // this._browserService.setInLocalStorage('getStartedShown',true); TODO [selfServe] - uncomment before publish, need to hide after one time
+              this._browserService.setInLocalStorage('getStartedShown',true);
               this._whatsNewWin.open();
           },200);
       }
@@ -51,18 +47,11 @@ export class DashboardComponent implements OnInit, AfterViewInit, OnDestroy {
         this._whatsNewWin.close();
     }
 
-    public unmute(): void {
-      if (window['loadedKalturaPlayer']) {
-          window['loadedKalturaPlayer'].muted = false;
-          this._showUnmuteBtn = false;
-      }
-    }
-
   ngAfterViewInit()
   {
     $(window).bind('resize', this.onResize); // We bind the event to a function reference that proxy 'actual' this inside
     this._resizeContent();
-    // this._showWhatsNew(); // TODO [selfServe] - uncomment when implementation is ready
+    this._showWhatsNew();
   }
 
   ngOnInit() {
