8d4fe0112a14389319cd63f71159c7a8314c2b4b
adir.ver <adir.ver@il-adirver-sup.com>
2022-03-14 11:59:58 +0200

    SUP-28797 use the real length of data to avoid mistmatch when exceedeing the char limit

--- src/applications/settings-reach-app/reach-profile/reach-profile-dictionary/reach-profile-dictionary-widget.service.ts
+++ src/applications/settings-reach-app/reach-profile/reach-profile-dictionary/reach-profile-dictionary-widget.service.ts
@@ -42,7 +42,7 @@ export class ReachProfileDictionaryWidget extends ReachProfileWidget {
         this.data.dictionaries.forEach((dictionary: KalturaDictionary) => {
             this._dictionaries.push(Object.assign({
                 words: dictionary.data.split(String.fromCharCode(10)),
-                usedCharacters: dictionary.data.split(String.fromCharCode(10)).join('').length,
+                usedCharacters: dictionary.data.length,
                 isValid: true
             }, dictionary));
         })
