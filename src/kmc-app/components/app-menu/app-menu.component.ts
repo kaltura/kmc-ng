013007011fec3ab484b387c425da5633d197f9e0
amirch1 <amir.chervinsky@kaltura.com>
2023-01-03 10:40:25 +0200

    chore: studio tab fix

--- src/kmc-app/components/app-menu/app-menu.component.ts
+++ src/kmc-app/components/app-menu/app-menu.component.ts
@@ -150,9 +150,6 @@ export class AppMenuComponent implements OnInit, OnDestroy {
             .pipe(cancelOnDestroy(this))
             .subscribe((event) => {
                 this.showSubMenu = true;
-                if (this.menuConfig) {
-                    this.showSubMenu = this.selectedMenuItem && this.selectedMenuItem.children && this.selectedMenuItem.children.length > 1;
-                }
                 this.hideMainMenu = false;
             });
 
