1d72d43dd1edeceb39c121233cda58e9d911ae4e
amirch1 <amir.chervinsky@kaltura.com>
2024-08-14 09:37:01 +0300

    fix(settings/media-repurposing): in notification templates use <br> instead of line breaks KMCNG-2661

--- src/applications/settings-mr-app/rule/actions/renderers/notification.templates.ts
+++ src/applications/settings-mr-app/rule/actions/renderers/notification.templates.ts
@@ -5,40 +5,8 @@ export const notificationTemplates = {
         executionSummary: 'Actions performed on queued entries'
     },
     body: {
-        profileScan: `Hi {user.firstName}
-
-On {scanDate}, {profileName} rule was run and new entries were added to your review queue.
-
-Please be sure to review your queue prior to {plannedExecutionDate}. For more information, a report is available for download in the Kaltura Management Console.
-
-
-Thank you,
-
-The Kaltura team
-
-
-
-This is a system generated message. Please do not reply to this email.`,
-        headsUp: `Hi {user.firstName}
-
-On {scanDate}, {profileName} rule was run, and new entries were added to your review queue. The review period is about to end, please be sure to review the entry queue prior to {plannedExecutionDate}.
-
-Thank you,
-
-The Kaltura team
-
-
-
-This is a system generated message. Please do not reply to this email.`,
-        executionSummary: `Hi {user.firstName}
-
-Following a review period of {profileName} rule, on {actualExecutionDate} actions were performed on entries in your account. For more information, a report is available for download in the Kaltura Management Console.
-
-
-Thank you,
-
-The Kaltura team
-
-This is a system generated message. Please do not reply to this email.`
+        profileScan: 'Hi {user.firstName}<br><br>On {scanDate}, {profileName} rule was run and new entries were added to your review queue.<br><br>Please be sure to review your queue prior to {plannedExecutionDate}. For more information, a report is available for download in the Kaltura Management Console.<br><br><br>Thank you,<br><br>The Kaltura team<br><br><br><br>This is a system generated message. Please do not reply to this email.',
+        headsUp: 'Hi {user.firstName}<br><br>On {scanDate}, {profileName} rule was run, and new entries were added to your review queue. The review period is about to end, please be sure to review the entry queue prior to {plannedExecutionDate}.<br><br><br>Thank you,<br><br>The Kaltura team<br><br><br><br>This is a system generated message. Please do not reply to this email.',
+        executionSummary: 'Hi {user.firstName}<br><br>Following a review period of {profileName} rule, on {actualExecutionDate} actions were performed on entries in your account. For more information, a report is available for download in the Kaltura Management Console.<br><br><br>Thank you,<br><br>The Kaltura team<br><br><br><br>This is a system generated message. Please do not reply to this email.'
     }
 }
