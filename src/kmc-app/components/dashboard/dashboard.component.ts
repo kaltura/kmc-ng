7449db164e11b14450a2ccc8f177dcea36167fcd
Amir Chervinsky <amir.chervinsky@kaltura.com>
2024-11-27 13:28:34 +0200

    feat: announcements widget KMCNG-2683 (#1085)
    * remove legacy what's new screen
    
    * updated build script
    
    * load checklist
    
    * update announcements configuration
    
    * fix announcements icon position

--- src/kmc-app/components/dashboard/dashboard.component.ts
+++ src/kmc-app/components/dashboard/dashboard.component.ts
@@ -1,33 +1,19 @@
-import { Component, OnInit, AfterViewInit, ViewChild, OnDestroy } from '@angular/core';
-import { AppAuthentication, AppShellService, BrowserService } from "app-shared/kmc-shell";
-import { buildDeployUrl } from 'config/server';
+import { Component, AfterViewInit, ViewChild, OnDestroy } from '@angular/core';
+import { AppShellService } from "app-shared/kmc-shell";
 import * as $ from 'jquery';
-import { PopupWidgetComponent } from '@kaltura-ng/kaltura-ui';
-import { Router } from "@angular/router";
 
 @Component({
   selector: 'kKMCDashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit, AfterViewInit, OnDestroy {
+export class DashboardComponent implements AfterViewInit, OnDestroy {
+
   @ViewChild('appMenu', { static: true }) private _appMenuRef : any;
-  @ViewChild('whatsNew', { static: true }) private _whatsNewWin : PopupWidgetComponent;
-  @ViewChild('studioPromo', { static: true }) private _studioPromo : PopupWidgetComponent;
 
-  // promo videos are hard coded to Kaltura production server in order to support additional environments
-  public _uiconf = 53016472;
-  public _pid = '811441';
-  public _cdnUrl = 'https://www.kaltura.com';
-
-  public _studioPlayerReady = false;
-  public _studioBannerUrl = buildDeployUrl('./assets/studio.png');
   private onResize : () => void;
 
-  constructor(private appShellService : AppShellService,
-              private appAuthentication: AppAuthentication,
-              private _browserService: BrowserService,
-              private _router: Router) {
+  constructor(private appShellService : AppShellService) {
       this.onResize = this._resizeContent.bind(this);
   }
 
@@ -40,53 +26,10 @@ export class DashboardComponent implements OnInit, AfterViewInit, OnDestroy {
     }
   }
 
-  private _showWhatsNew(): void {
-      const isSelfserve = this.appAuthentication.appUser.partnerInfo.isSelfServe;
-      const whatsNewShown = this._browserService.getFromLocalStorage('getStartedShown') || false;
-      if (isSelfserve && !whatsNewShown){
-          setTimeout(()=>{
-              this._browserService.setInLocalStorage('getStartedShown',true);
-              this._whatsNewWin.open();
-          },200);
-      }
-  }
-  private _showStudioPromo(): void {
-      const isSelfserve = this.appAuthentication.appUser.partnerInfo.isSelfServe;
-      const studioPromoShown = this._browserService.getFromLocalStorage('studioPromoShown') || false;
-      if (!isSelfserve && !studioPromoShown){
-          setTimeout(()=>{
-              this._browserService.setInLocalStorage('studioPromoShown',true);
-              this._studioPromo.open();
-          },200);
-      }
-  }
-
-  public onStudioPlayerReady(player: any): void {
-      player.addEventListener(player.Event.Core.FIRST_PLAY, event => {
-          setTimeout(() => {
-            this._studioPlayerReady = true;
-          },1000);
-      });
-  }
-
-  public navigateToStudio(): void {
-      this._studioPromo.close();
-      this._router.navigateByUrl('/studio/v3');
-  }
-
-    closeWin():void{
-        this._whatsNewWin.close();
-    }
-
   ngAfterViewInit()
   {
     $(window).bind('resize', this.onResize); // We bind the event to a function reference that proxy 'actual' this inside
     this._resizeContent();
-    this._showWhatsNew();
-    this._showStudioPromo();
-  }
-
-  ngOnInit() {
   }
 
   ngOnDestroy(){
