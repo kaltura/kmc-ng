b86dd7cc09c8673819bbb620c81923ecd3ec0bd6
amirch1 <amir.chervinsky@kaltura.com>
2025-08-19 09:36:06 +0300

    bump kmc version to v7.31.4

--- src/configuration/global-config.ts
+++ src/configuration/global-config.ts
@@ -29,7 +29,7 @@ export interface GlobalConfig {
 export const globalConfig: GlobalConfig = {
     client: {
         production: environment.production,
-        appVersion: '7.31.3',
+        appVersion: '7.31.4',
         useSecuredProtocol: environment.client.useSecuredProtocol,
         countriesList: countryCodes,
         views: {
