e0cd980424c4a965962211f076c2c726ca41555b
amirch1 <amir.chervinsky@kaltura.com>
2025-12-28 16:30:29 +0200

    chore: updated error page

--- src/kmc-app/components/error/error.component.ts
+++ src/kmc-app/components/error/error.component.ts
@@ -11,32 +11,13 @@ export class ErrorComponent implements OnDestroy{
     constructor(private _browserService: BrowserService) {
         if (typeof this._browserService.previousRoute === 'undefined') {
             this._browserService.navigateToDefault();
-        }else{
+        } else {
             document.body.style.overflowY = 'hidden';
             this.showPage = true;
         }
     }
 
     showPage = false;
-    eggReady = false;
-
-    private prepareEgg = {
-        eye1: false,
-        eye2: false,
-        ear1: false,
-        ear2: false
-    }
-
-    public _updateEgg(key: string) {
-        this.prepareEgg[key] = true;
-        this.eggReady = Object.keys(this.prepareEgg).every((key) => {
-            return this.prepareEgg[key]
-        });
-    }
-
-    public openLink(link) {
-        window.open(link, '_blank');
-    }
 
     ngOnDestroy(){
         document.body.style.overflowY = 'auto';
